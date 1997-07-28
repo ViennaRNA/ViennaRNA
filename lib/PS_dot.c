66676e5465b170bfc930beb3163e4e736ecd0b69
ivo <ivo>
1997-07-28 18:59:22 +0000

    PS_rna_plot now takes structure as argument, insead of using base_pair[]

--- lib/PS_dot.c
+++ lib/PS_dot.c
@@ -5,7 +5,7 @@
 		 c Ivo L Hofacker and Peter F Stadler
 			  Vienna RNA package
 */
-/*Last changed Time-stamp: <96/05/06 00:05:57 ivo> */
+/*Last changed Time-stamp: <97/07/28 20:59:02 ivo> */
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
@@ -14,19 +14,23 @@
 
 #define  PRIVATE   static
 #define  MAX(A,B)    (A)>(B)?(A):(B)
-#define  MAXLENGTH 1500
-#define  PI       3.141592654  
+#define  MAXLENGTH 2000
+#define  PI       3.141592654
 #define  PIHALF       PI/2.
 
 #define  BRANCH     30
 
+PRIVATE void   make_pair_table(char *structure, short *table);
+PRIVATE short  *pair_table;
 /*--------------------------------------------------------------------------*/
 
 void PS_dot_plot(char *string, char *wastlfile)
 {
+   /* produce PostScript dot plot from probabilities in pr[] array */
+   
    FILE *wastl;
    int i, j, length;
-   double tmp,scale;
+   double tmp;
    
    length= strlen(string);
    wastl = fopen(wastlfile,"w");
@@ -34,16 +38,18 @@ void PS_dot_plot(char *string, char *wastlfile)
    
    fprintf(wastl,"%%!PS-Adobe-2.0 EPSF-1.2\n");
    fprintf(wastl,"%%%%Title: RNA DotPlot\n");
-   fprintf(wastl,"%%%%Creator: RNAfold V.01.002c  - by Oymolon\n");
+   fprintf(wastl,"%%%%Creator: PS_dot.c, ViennaRNA package\n");
    fprintf(wastl,"%%%%CreationDate: %s", time_stamp());
    fprintf(wastl,"%%%%BoundingBox: 66 211 518 662\n");
+   fprintf(wastl,"%%%%DocumentFonts: Times-Roman\n");
    fprintf(wastl,"%%%%Pages: 1\n");
-   fprintf(wastl,"%%%%EndComments: blah blah...\n\n");
+   fprintf(wastl,"%%%%EndComments\n\n");
    
    fprintf(wastl,"%%This file contains the square roots "
 	   "of the base pair probabilities in the form\n");
    fprintf(wastl,"%% i  j  sqrt(p(i,j)) ubox\n");
-
+   
+   fprintf(wastl,"100 dict begin\n");  /* DSC says EPS should create a dict */
    fprintf(wastl,"/ubox {\n");     /* upper triangle matrix */
    fprintf(wastl,"   3 1 roll\n");
    fprintf(wastl,"   exch len exch sub 1 add box\n");
@@ -139,11 +145,14 @@ void PS_dot_plot(char *string, char *wastlfile)
 	      base_pair[i].i, base_pair[i].j); 
 
    fprintf(wastl,"showpage\n");
+   fprintf(wastl,"end\n");
+   fprintf(wastl,"%%%%EOF\n");
    fclose(wastl);
 }
 
 /*---------------------------------------------------------------------------*/
 
+#if 0
 PRIVATE int pairs(int i)
 {
     struct bond *n;
@@ -154,6 +163,7 @@ PRIVATE int pairs(int i)
     }
     return(0);
 }
+#endif
 
 /*---------------------------------------------------------------------------*/
 
@@ -184,7 +194,7 @@ static void loop(int i, int j)
 			       previous pair for correct while-loop
 			       termination.  */
     while (i != j) {
-	partner = pairs(i);
+	partner = pair_table[i];
 	if (!partner)
 	    i++, count++, bubble++;
 	else {
@@ -199,20 +209,20 @@ static void loop(int i, int j)
 	    do {
 		k++, l--, ladder++;        /* go along the stack region */
 	    }
-	    while (pairs(k) == l);
+	    while (pair_table[k] == l);
 
 	    fill = ladder-2;
 	    if (ladder >= 2) {
-		angle[start_k+1+fill] += PIHALF;   /*  Loop entries and    */
-		angle[start_l-1-fill] += PIHALF;   /*  exits get an        */
-		angle[start_k]        += PIHALF;   /*  additional PI/2.    */
-		angle[start_l]        += PIHALF;   /*  Why ? (exercise)    */
-		if (ladder > 2) {
-		    for (; fill >= 1; fill--) {
-			angle[start_k+fill] = PI;  /*  fill in the angles  */
-			angle[start_l-fill] = PI;  /*  for the backbone    */
-		    }
-		}
+	       angle[start_k+1+fill] += PIHALF;   /*  Loop entries and    */
+	       angle[start_l-1-fill] += PIHALF;   /*  exits get an        */
+	       angle[start_k]        += PIHALF;   /*  additional PI/2.    */
+	       angle[start_l]        += PIHALF;   /*  Why ? (exercise)    */
+	       if (ladder > 2) {
+		  for (; fill >= 1; fill--) {
+		     angle[start_k+fill] = PI;  /*  fill in the angles  */
+		     angle[start_l-fill] = PI;  /*  for the backbone    */
+		  }
+	       }
 	    }
 	    stack_size[++stk] = ladder;
 	    loop(k, l);
@@ -234,7 +244,7 @@ static void loop(int i, int j)
 
 /*---------------------------------------------------------------------------*/
 #define PRINT_SIZES 0
-static void parse(char *string)
+static void parse(int len)
 {
     int k;
 
@@ -254,7 +264,7 @@ static void parse(char *string)
        stack_size[i], i=1,...,stk     number of pairs in the i-th stack
     */
 
-    loop(0, strlen(string)+1);
+    loop(0, len+1);
 
     loop_size[lp] -= 2;     /* correct for cheating with function loop */
 #if PRINT_SIZES
@@ -272,18 +282,23 @@ static void parse(char *string)
 #define INIT_Y       100.     /* see above */
 #define RADIUS 15.
 
-void PS_rna_plot(char *string, char *ssfile)
+#define pairs(I)  (((I)>0)?(((I)<length)?(pair_table[(I)-1]+1):0):0)
+void PS_rna_plot(char *string, char *structure, char *ssfile)
 {
-   
   float  alpha, xmin, xmax, ymin, ymax, size;
   int    i, length;
   FILE  *xyplot;
 
-  if (strlen(string)>MAXLENGTH) {
-     fprintf(stderr,"structure too long, not doing xy_plot\n");
+  length = strlen(string);
+  if (length>MAXLENGTH) {
+     fprintf(stderr,"INFO: structure too long, not doing xy_plot\n");
      return;
   }
-  parse(string);
+
+  pair_table = (short *) space((length+1)*sizeof(short));
+  make_pair_table(structure, pair_table);
+  
+  parse(length);
   
   alpha = INIT_ANGLE;
   x[0]  = INIT_X;
@@ -291,8 +306,6 @@ void PS_rna_plot(char *string, char *ssfile)
   xmin = xmax = x[0];
   ymin = ymax = y[0];
 
-  length = strlen(string);
-  
   for (i = 1; i <= length; i++) {
      x[i] = x[i-1]+RADIUS*cos(alpha);
      y[i] = y[i-1]+RADIUS*sin(alpha);
@@ -317,16 +330,20 @@ void PS_rna_plot(char *string, char *ssfile)
 	  "%%%%BoundingBox: 66 210 518 662\n"
 	  "%%%%DocumentFonts: Courier\n"
 	  "%%%%Pages: 1\n"
-	  "%%%%EndComments\n", time_stamp());
+	  "%%%%EndComments\n\n", time_stamp());
 
+  fprintf(xyplot,"100 dict begin\n");  /* DSC says EPS should create a dict */
   fprintf(xyplot,
-	  "/smurgl { lineto currentpoint -4.5 dup rmoveto\n"
-	  "          3 -1 roll show moveto } def\n");
+	  "/fsize  {15} def\n"
+	  "/cshow  { dup stringwidth pop fsize neg 3 div exch neg 2 div exch\n"
+	  "          rmoveto show} def\n"
+	  "/smurgl { lineto currentpoint 3 -1 roll cshow moveto } def\n");
+
   fprintf(xyplot, "72 216 translate\n");
   fprintf(xyplot, "72 6 mul %3.3f div dup scale\n", size);
   fprintf(xyplot, "%4.3f %4.3f translate\n",
 	  (size-xmin-xmax)/2, (size-ymin-ymax)/2);
-  fprintf(xyplot, "/Courier findfont 15 scalefont setfont\n");
+  fprintf(xyplot, "/Courier findfont fsize scalefont setfont\n");
   fprintf(xyplot, "100 100 moveto\n");
   
   for (i = 0; i < length; i++) 
@@ -334,5 +351,45 @@ void PS_rna_plot(char *string, char *ssfile)
 
   fprintf(xyplot, "stroke\n");
   fprintf(xyplot, "showpage\n");
+  fprintf(xyplot, "end\n");
+  fprintf(xyplot, "%%%%EOF\n");
   fclose(xyplot);
+
+  free(pair_table);
+}
+
+/*---------------------------------------------------------------------------*/
+
+PRIVATE void make_pair_table(char *structure, short *table)
+{
+   int i,j,hx;
+   short *stack;
+   
+   hx=0;
+   stack = (short *) space(sizeof(short)*(strlen(structure)+1));
+             
+   for (i=1; i<=strlen(structure); i++) {
+      switch (structure[i-1]) {
+       case '(': 
+	 stack[hx++]=i;
+	 break;
+       case ')':
+	 j = stack[--hx];
+	 if (hx<0) {
+	    fprintf(stderr, "%s\n", structure);
+	    nrerror("unbalanced brackets in make_pair_table");
+	 }
+	 table[i]=j;
+	 table[j]=i;
+	 break;
+       default:   /* unpaired base, usually '.' */
+	 table[i]= 0;
+	 break;
+      }
+   }
+   if (hx!=0) {
+      fprintf(stderr, "%s\n", structure);
+      nrerror("unbalanced brackets in make_pair_table");
+   }
+   free(stack);
 }
