f63655af7e657c30f8f8fed7519434f0a582a9eb
ivo <ivo>
1997-10-01 16:43:48 +0000

    added free_profile()

--- H/profiledist.h
+++ H/profiledist.h
@@ -2,4 +2,4 @@ extern float    profile_edit_distance(float **T1, float **T2);
 extern float  **Make_bp_profile(int length);
 extern void     sprint_aligned_bppm(float **T1, float **T2);
 extern void     print_bppm(float **T);
-
+extern void     free_profile(float **T);
