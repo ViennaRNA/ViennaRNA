5b18d549b1bf5273b6c59d18b0b30145c8fd0e9b
ivo <ivo>
1997-10-02 17:29:10 +0000

    includes dist_vars.h when needed

--- H/stringdist.h
+++ H/stringdist.h
@@ -1,2 +1,5 @@
+#ifndef DIST_VARS_H
+#include "dist_vars.h"  /* defines the type Tree */
+#endif
 extern float     string_edit_distance(swString *T1, swString *T2);
 extern swString *Make_swString(char *string);
--- H/treedist.h
+++ H/treedist.h
@@ -1,3 +1,6 @@
+#ifndef DIST_VARS_H
+#include "dist_vars.h"  /* defines the type Tree */
+#endif
 extern  Tree   *make_tree(char *struc);
 extern  float   tree_edit_distance(Tree *T1, Tree *T2);
 extern  void    print_tree(Tree *t);
