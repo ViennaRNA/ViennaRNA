f9a1906a7ede1ba38f62ac4e623da154cf323c77
ivo <ivo>
1997-11-03 10:39:43 +0000

    *** empty log message ***

--- lib/treedist.c
+++ lib/treedist.c
@@ -3,15 +3,16 @@
 		Walter Fontana, Ivo L Hofacker, Peter F Stadler
 			     Vienna RNA Package
 */
-/* Last changed Time-stamp: <97/10/27 14:10:47 ivo> */
+/* Last changed Time-stamp: <97/10/27 15:23:48 ivo> */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 #include  "edit_cost.h"
 #include  "dist_vars.h"
 #include  "utils.h"
-static char rcsid[] = "$Id: treedist.c,v 1.2 1997/10/27 13:10:54 ivo Exp $";
+static char rcsid[] = "$Id: treedist.c,v 1.3 1997/11/03 10:39:43 ivo Rel $";
 
 #define PRIVATE  static
 #define PUBLIC
