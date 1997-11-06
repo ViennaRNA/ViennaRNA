b15d25077f2001c59f0cb1db8cac58ce37ba981c
ivo <ivo>
1997-11-06 17:13:23 +0000

    *** empty log message ***

--- H/PS_dot.h
+++ H/PS_dot.h
@@ -1,2 +1,4 @@
 extern void PS_rna_plot(char *string, char *structure, char *file);
+extern void gmlRNA(char *string, char *structure, char *ssfile, char option);
 extern void PS_dot_plot(char *string, char *file);
+
