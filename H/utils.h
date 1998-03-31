96b54c1fca1bd88dd104e2a79daff0956c9301f4
ivo <ivo>
1998-03-31 21:36:00 +0000

    *** empty log message ***

--- H/utils.h
+++ H/utils.h
@@ -10,19 +10,19 @@ extern void   filecopy(FILE *from, FILE *to); /* inefficient `cp' */
 extern char  *time_stamp(void);               /* current date in a string */
 extern char  *random_string(int l, const char symbols[]);
 /* random string of length l using characters from symbols[] */
-extern int    hamming(const char s1[], const char s2[]);
+extern int    hamming(const char *s1, const char *s2);
 /* calculate hamming distance */
 extern char  *get_line(FILE *fp); /* read one (arbitrary length) line from fp */
 
 
-extern unsigned char *pack_structure(char *struc);
+extern unsigned char *pack_structure(const char *struc);
 /* pack secondary secondary structure, 5:1 compression using base 3 encoding */
-extern char  *unpack_structure(unsigned char *packed);
+extern char  *unpack_structure(const unsigned char *packed);
 /* unpack sec structure packed with pack_structure() */
-extern short *make_pair_table(char *structure);
+extern short *make_pair_table(const char *structure);
 /* returns a newly allocated table, such that:  table[i]=j if (i.j) pair or
    0 if i is unpaired, table[0] contains the length of the structure. */
 
-extern int bp_distance(char *str1, char *str2);
+extern int bp_distance(const char *str1, const char *str2);
 /* dist = {number of base pairs in one structure but not in the other} 
    same as edit distance with open-pair close-pair as move-set */
