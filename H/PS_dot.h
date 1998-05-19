4da333ce2fc6a9de7f1d4d592a02065a65bb56a7
ivo <ivo>
1998-05-19 17:43:44 +0000

    add ssv_rna_plot() and rna_plot_type (for selecting naview or simple)

--- H/PS_dot.h
+++ H/PS_dot.h
@@ -3,5 +3,8 @@ extern int PS_rna_plot(char *string, char *structure, char *file);
 /* write PostScript drawing of structure to file */
 extern int gmlRNA(char *string, char *structure, char *ssfile, char option);
 /* structure drawing in gml */
+extern int ssv_rna_plot(char *string, char *structure, char *ssfile);
+/*write coord file for SStructView */
 extern int PS_dot_plot(char *string, char *file);
 /* produce a PostScript dot plot of the pair probability matix */
+extern int rna_plot_type;   /* 0= simple coordinates, 1= naview */
