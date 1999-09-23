93048252a77a97e98bb76b70ecbd9346d64b86d1
ivo <ivo>
1999-09-23 00:25:56 +0000

    added xrna_plot()

--- H/PS_dot.h
+++ H/PS_dot.h
@@ -5,6 +5,8 @@ extern int gmlRNA(char *string, char *structure, char *ssfile, char option);
 /* structure drawing in gml */
 extern int ssv_rna_plot(char *string, char *structure, char *ssfile);
 /*write coord file for SStructView */
+extern int xrna_plot(char *string, char *structure, char *ssfile);
+/*write .ss file for further editing in XRNA */
 extern int PS_dot_plot(char *string, char *file);
 /* produce a PostScript dot plot of the pair probability matix */
 extern int rna_plot_type;   /* 0= simple coordinates, 1= naview */
