b8a417179d7fa11182ad419536c14ba64331694c
ivo <ivo>
2000-09-28 12:31:55 +0000

    Cosmetics to make mostly lint clean;  -d3 option

--- Progs/RNAfold.c
+++ Progs/RNAfold.c
@@ -1,9 +1,9 @@
-/* Last changed Time-stamp: <2000-08-14 20:50:23 ivo> */
+/* Last changed Time-stamp: <2000-09-28 14:31:01 ivo> */
 /*                
-		Ineractive Access to folding Routines
+		  Ineractive Access to folding Routines
 
-			   c Ivo L Hofacker
-			  Vienna RNA package
+		  c Ivo L Hofacker
+		  Vienna RNA package
 */
 
 #include <stdio.h>
@@ -20,7 +20,7 @@
 extern void  read_parameter_file(const char fname[]);
 
 /*@unused@*/
-static char rcsid[] = "$Id: RNAfold.c,v 1.12 2000/08/14 18:53:26 ivo Exp $";
+static char rcsid[] = "$Id: RNAfold.c,v 1.13 2000/09/28 12:31:55 ivo Rel $";
 
 #define PRIVATE static
 
@@ -29,227 +29,229 @@ static char  scale2[] = "....,....5....,....6....,....7....,....8";
 
 PRIVATE void usage(void);
 
-static struct bond  *bp, *bpp;
-
-
 /*--------------------------------------------------------------------------*/
 
 int main(int argc, char *argv[])
 {
-   char *string, *line;
-   char *structure=NULL, *cstruc=NULL;
-   char  fname[13], ffname[20], gfname[20];
-   char  *ParamFile=NULL;
-   char  *ns_bases=NULL, *c;
-   int   i, length, l, sym, r;
-   float energy, min_en;
-   float kT, sfact=1.07;
-   int   pf=0, istty;
-   int noconv=0;
+  char *string, *line;
+  char *structure=NULL, *cstruc=NULL;
+  char  fname[13], ffname[20], gfname[20];
+  char  *ParamFile=NULL;
+  char  *ns_bases=NULL, *c;
+  int   i, length, l, sym, r;
+  double energy, min_en;
+  double kT, sfact=1.07;
+  int   pf=0, istty;
+  int noconv=0;
    
-   do_backtrack = 1; 
-   string=NULL;
-   for (i=1; i<argc; i++) {
-      if (argv[i][0]=='-') 
-	switch ( argv[i][1] )
-	  {
-	  case 'T':  if (argv[i][2]!='\0') usage();
-	    if(i==argc-1) usage();
-	    r=sscanf(argv[++i], "%lf", &temperature);
-	    if (!r) usage();
-	    break;
-	  case 'p':  pf=1;
-	    if (argv[i][2]!='\0')
-	      (void) sscanf(argv[i]+2, "%d", &do_backtrack);
-	    break;
-	  case 'n':
-	    if ( strcmp(argv[i], "-noGU")==0) noGU=1;
-	    if ( strcmp(argv[i], "-noCloseGU")==0) no_closingGU=1;
-	    if ( strcmp(argv[i], "-noLP")==0) noLonelyPairs=1;
-	    if ( strcmp(argv[i], "-nsp") ==0) {
-	      if (i==argc-1) usage();
-	      ns_bases = argv[++i];
-	    }
-	    if ( strcmp(argv[i], "-noconv")==0) noconv=1;
-	    break;
-	  case '4':
-	    tetra_loop=0;
-	    break;
-	  case 'e':
-	    if(i==argc-1) usage();
-	    r=sscanf(argv[++i],"%d", &energy_set);
-	    if (!r) usage();
-	    break;
-	  case 'C':
-	    fold_constrained=1;
-	    break;
-	  case 'S':
-	    if(i==argc-1) usage();
-	    r=sscanf(argv[++i],"%f", &sfact);
-	    if (!r) usage();
-	    break;
-	  case 'd': dangles=0;
-	    if (strcmp(argv[i],"-d2")==0) dangles=2;
-	    if (strcmp(argv[i],"-d1")==0) dangles=1;
-	    break;
-	  case 'P':
+  do_backtrack = 1; 
+  string=NULL;
+  for (i=1; i<argc; i++) {
+    if (argv[i][0]=='-') 
+      switch ( argv[i][1] )
+	{
+	case 'T':  if (argv[i][2]!='\0') usage();
+	  if(i==argc-1) usage();
+	  r=sscanf(argv[++i], "%lf", &temperature);
+	  if (!r) usage();
+	  break;
+	case 'p':  pf=1;
+	  if (argv[i][2]!='\0')
+	    (void) sscanf(argv[i]+2, "%d", &do_backtrack);
+	  break;
+	case 'n':
+	  if ( strcmp(argv[i], "-noGU")==0) noGU=1;
+	  if ( strcmp(argv[i], "-noCloseGU")==0) no_closingGU=1;
+	  if ( strcmp(argv[i], "-noLP")==0) noLonelyPairs=1;
+	  if ( strcmp(argv[i], "-nsp") ==0) {
 	    if (i==argc-1) usage();
-	    ParamFile = argv[++i];
-	    break;
-	  default: usage();
-	  } 
-   }
+	    ns_bases = argv[++i];
+	  }
+	  if ( strcmp(argv[i], "-noconv")==0) noconv=1;
+	  break;
+	case '4':
+	  tetra_loop=0;
+	  break;
+	case 'e':
+	  if(i==argc-1) usage();
+	  r=sscanf(argv[++i],"%d", &energy_set);
+	  if (!r) usage();
+	  break;
+	case 'C':
+	  fold_constrained=1;
+	  break;
+	case 'S':
+	  if(i==argc-1) usage();
+	  r=sscanf(argv[++i],"%lf", &sfact);
+	  if (!r) usage();
+	  break;
+	case 'd': dangles=0;
+	  if (argv[i][2]!='\0') {
+	    r=sscanf(argv[i]+2, "%d", &dangles);
+	    if (r!=1) usage();
+	  }
+	  break;
+	case 'P':
+	  if (i==argc-1) usage();
+	  ParamFile = argv[++i];
+	  break;
+	default: usage();
+	} 
+  }
 
-   if (ParamFile != NULL)
-     read_parameter_file(ParamFile);
+  if (ParamFile != NULL)
+    read_parameter_file(ParamFile);
    
-   if (ns_bases != NULL) {
-      nonstandards = space(33);
-      c=ns_bases;
-      i=sym=0;
-      if (*c=='-') {
-	 sym=1; c++;
+  if (ns_bases != NULL) {
+    nonstandards = space(33);
+    c=ns_bases;
+    i=sym=0;
+    if (*c=='-') {
+      sym=1; c++;
+    }
+    while (*c!='\0') {
+      if (*c!=',') {
+	nonstandards[i++]=*c++;
+	nonstandards[i++]=*c;
+	if ((sym)&&(*c!=*(c-1))) {
+	  nonstandards[i++]=*c;
+	  nonstandards[i++]=*(c-1);
+	}
       }
-      while (*c) {
-	 if (*c!=',') {
-	    nonstandards[i++]=*c++;
-	    nonstandards[i++]=*c;
-	    if ((sym)&&(*c!=*(c-1))) {
-	       nonstandards[i++]=*c;
-	       nonstandards[i++]=*(c-1);
-	    }
-	 }
-	 c++;
-      }
-   }
-   istty = isatty(fileno(stdout))&&isatty(fileno(stdin));
-   if ((fold_constrained)&&(istty)) {
-      printf("Input constraints using the following notation:\n");
-      printf("| : paired with another base\n");
-      printf(". : no constraint at all\n");
-      printf("x : base must not pair\n");
-      printf("< : base i is paired with a base j<i\n");
-      printf("> : base i is paired with a base j>i\n");
-      printf("matching brackets ( ): base i pairs base j\n");
-   } 
+      c++;
+    }
+  }
+  istty = isatty(fileno(stdout))&&isatty(fileno(stdin));
+  if ((fold_constrained)&&(istty)) {
+    printf("Input constraints using the following notation:\n");
+    printf("| : paired with another base\n");
+    printf(". : no constraint at all\n");
+    printf("x : base must not pair\n");
+    printf("< : base i is paired with a base j<i\n");
+    printf("> : base i is paired with a base j>i\n");
+    printf("matching brackets ( ): base i pairs base j\n");
+  } 
 	
-   do {				/* main loop: continue until end of file */
-      if (istty) {
-	 printf("\nInput string (upper or lower case); @ to quit\n");
-	 printf("%s%s\n", scale1, scale2);
-      }
-      fname[0]='\0';
-      if ((line = get_line(stdin))==NULL) break;
+  do {				/* main loop: continue until end of file */
+    if (istty) {
+      printf("\nInput string (upper or lower case); @ to quit\n");
+      printf("%s%s\n", scale1, scale2);
+    }
+    fname[0]='\0';
+    if ((line = get_line(stdin))==NULL) break;
 
-      /* skip comment lines and get filenames */
-      while ((*line=='*')||(*line=='\0')||(*line=='>')) {
-	 if (*line=='>')
-	   (void) sscanf(line, ">%12s", fname);
-	 printf("%s\n", line);
-	 free(line);
-	 if ((line = get_line(stdin))==NULL) break;
-      } 
-
-      if ((line ==NULL) || (strcmp(line, "@") == 0)) break;
-
-      string = (char *) space(strlen(line)+1);
-      (void) sscanf(line,"%s",string);
+    /* skip comment lines and get filenames */
+    while ((*line=='*')||(*line=='\0')||(*line=='>')) {
+      if (*line=='>')
+	(void) sscanf(line, ">%12s", fname);
+      printf("%s\n", line);
       free(line);
-      length = strlen(string);
+      if ((line = get_line(stdin))==NULL) break;
+    } 
 
-      structure = (char *) space((unsigned) length+1);
-      if (fold_constrained) {
-	cstruc = get_line(stdin);
-	strncpy(structure, cstruc, length+1);
-      }
-      for (l = 0; l < length; l++) {
-	string[l] = toupper(string[l]);
-	if (!noconv && string[l] == 'T') string[l] = 'U';
-      }
-      if (istty)
-	 printf("length = %d\n", length);
+    if ((line ==NULL) || (strcmp(line, "@") == 0)) break;
 
-      initialize_fold(length);
-      min_en = fold(string, structure);
-      printf("%s\n%s", string, structure);
-      if (istty)
-	 printf("\n minimum free energy = %6.2f kcal/mol\n", min_en);
+    string = (char *) space(strlen(line)+1);
+    (void) sscanf(line,"%s",string);
+    free(line);
+    length = (int) strlen(string);
+
+    structure = (char *) space((unsigned) length+1);
+    if (fold_constrained) {
+      cstruc = get_line(stdin);
+      if (cstruc!=NULL) 
+	strncpy(structure, cstruc, length);
       else
-	 printf(" (%6.2f)\n", min_en);
-       
-      fflush(stdout);
-       
-      if (fname[0]!='\0') {
-	 strcpy(ffname, fname);
-	 strcat(ffname, "_ss.ps");
-	 strcpy(gfname, fname);
-	 strcat(gfname, "_ss.g");
-      } else {
-	 strcpy(ffname, "rna.ps");
-	 strcpy(gfname, "rna.g");
-      }
-      if (length<2000)
-	PS_rna_plot(string, structure, ffname);
-      else 
-	fprintf(stderr,"INFO: structure too long, not doing xy_plot\n");
+	fprintf(stderr, "constraints missing\n");
+    }
+    for (l = 0; l < length; l++) {
+      string[l] = toupper(string[l]);
+      if (!noconv && string[l] == 'T') string[l] = 'U';
+    }
+    if (istty)
+      printf("length = %d\n", length);
 
-      bp = base_pair;
-      bpp= space(16);
-      base_pair=bpp;
-      free_arrays();
+    initialize_fold(length);
+    min_en = fold(string, structure);
+    printf("%s\n%s", string, structure);
+    if (istty)
+      printf("\n minimum free energy = %6.2f kcal/mol\n", min_en);
+    else
+      printf(" (%6.2f)\n", min_en);
+       
+    (void) fflush(stdout);
+       
+    if (fname[0]!='\0') {
+      strcpy(ffname, fname);
+      strcat(ffname, "_ss.ps");
+      strcpy(gfname, fname);
+      strcat(gfname, "_ss.g");
+    } else {
+      strcpy(ffname, "rna.ps");
+      strcpy(gfname, "rna.g");
+    }
+    if (length<2000)
+      (void) PS_rna_plot(string, structure, ffname);
+    else 
+      fprintf(stderr,"INFO: structure too long, not doing xy_plot\n");
+
+    { /* free mfe arrays but preserve base_pair for PS_dot_plot */
+      struct bond  *bp;
+      bp = base_pair; base_pair = space(16);
+      free_arrays();  /* free's base_pair */
       base_pair = bp;
-       
-      if (pf) {
+    } 
+    if (pf) {
 
-	 if (dangles==1) {
-	   dangles=2;   /* recompute with dangles as in pf_fold() */
-	   min_en = energy_of_struct(string, structure);
-	   dangles=1;
-	 }
-	 
-	 kT = (temperature+273.15)*1.98717/1000.; /* in Kcal */
-	 pf_scale = exp(-(sfact*min_en)/kT/length);
-	 if (length>2000) fprintf(stderr, "scaling factor %f\n", pf_scale);
-
-	 init_pf_fold(length);
-
-	 if (fold_constrained)
-	    strncpy(structure, cstruc, length+1);
-	 energy = pf_fold(string, structure);
-	 
-	 if (do_backtrack) {
-	    printf("%s", structure);
-	    if (!istty) printf(" [%6.2f]\n", energy);
-	    else printf("\n");
-	 }
-	 if ((istty)||(!do_backtrack)) 
-	    printf(" free energy of ensemble = %6.2f kcal/mol\n", energy);
-	 printf(" frequency of mfe structure in ensemble %g\n",
-		exp((energy-min_en)/kT));
-	 
-	 if (do_backtrack) {
-	    if (fname[0]!='\0') {
-	       strcpy(ffname, fname);
-	       strcat(ffname, "_dp.ps");
-	    } else strcpy(ffname, "dot.ps");
-	    PS_dot_plot(string, ffname);
-	 }
-	 free_pf_arrays();
-
+      if (dangles==1) {
+	dangles=2;   /* recompute with dangles as in pf_fold() */
+	min_en = energy_of_struct(string, structure);
+	dangles=1;
       }
-      if (fold_constrained) free(cstruc);
-      free(base_pair);
-      fflush(stdout);
-      free(string);
-      free(structure); 
-   } while (1);
-   return 0;
+	 
+      kT = (temperature+273.15)*1.98717/1000.; /* in Kcal */
+      pf_scale = exp(-(sfact*min_en)/kT/length);
+      if (length>2000) fprintf(stderr, "scaling factor %f\n", pf_scale);
+
+      init_pf_fold(length);
+
+      if (cstruc!=NULL)
+	strncpy(structure, cstruc, length+1);
+      energy = pf_fold(string, structure);
+	 
+      if (do_backtrack) {
+	printf("%s", structure);
+	if (!istty) printf(" [%6.2f]\n", energy);
+	else printf("\n");
+      }
+      if ((istty)||(!do_backtrack)) 
+	printf(" free energy of ensemble = %6.2f kcal/mol\n", energy);
+      printf(" frequency of mfe structure in ensemble %g\n",
+	     exp((energy-min_en)/kT));
+	 
+      if (do_backtrack) {
+	if (fname[0]!='\0') {
+	  strcpy(ffname, fname);
+	  strcat(ffname, "_dp.ps");
+	} else strcpy(ffname, "dot.ps");
+	(void) PS_dot_plot(string, ffname);
+      }
+      free_pf_arrays();
+
+    }
+    if (cstruc!=NULL) free(cstruc);
+    free(base_pair);
+    (void) fflush(stdout);
+    free(string);
+    free(structure); 
+  } while (1);
+  return 0;
 }
 
 PRIVATE void usage(void)
 {
-   nrerror("usage:\n"
-	   "RNAfold [-p[0]] [-C] [-T temp] [-4] [-d[2]] [-noGU] [-noCloseGU]\n" 
-	   "        [-noLP] [-e e_set] [-P paramfile] [-nsp pairs] [-S scale] "
-	   "[-noconv]\n");
+  nrerror("usage:\n"
+	  "RNAfold [-p[0]] [-C] [-T temp] [-4] [-d[2|3]] [-noGU] [-noCloseGU]\n" 
+	  "        [-noLP] [-e e_set] [-P paramfile] [-nsp pairs] [-S scale] "
+	  "[-noconv]\n");
 }
