5525eb2a0325952b21650cc3a823096f25c6596f
ivo <ivo>
2000-10-10 09:25:59 +0000

    added option_string for use e.g. in PS_dot.c

--- lib/fold_vars.c
+++ lib/fold_vars.c
@@ -1,8 +1,10 @@
-/* Last changed Time-stamp: <1998-07-07 16:43:54 ivo> */
+/* Last changed Time-stamp: <2000-10-10 11:24:26 ivo> */
 /*
        global variables to change behaviour of folding routines
 			  Vienna RNA package
 */
+#include <string.h>
+#include <stdio.h>
 #include "fold_vars.h"
 
 int  noGU = 0;           /* GU not allowed at all */
@@ -11,17 +13,30 @@ int  tetra_loop = 1;     /* Fold with specially stable 4-loops */
 int  energy_set = 0;     /* 0 = BP; 1=any with GC; 2=any with AU parameters */
 int  dangles = 1;	 /* use dangling end energies */
 char *nonstandards = (char *)0; /* contains allowed non standard bases */
-float temperature = 37.0;
+double temperature = 37.0;
 int  james_rule = 1;     /* interior loops of size 2 get energy 0.8Kcal and
-			    no mismatches */
+			    no mismatches (no longer used) */
 struct bond  *base_pair;
 
-FLT_OR_DBL *pr;           /* base pairing prob. matrix */
-int  *iindx;              /* pr[i,j] -> pr[iindx[i]-j] */
-float pf_scale=- 1;       /* scaling factor to avoid floting point overflows */
-int   fold_constrained = 0; /* guess what */
+FLT_OR_DBL *pr;          /* base pairing prob. matrix */
+int  *iindx;             /* pr[i,j] -> pr[iindx[i]-j] */
+double pf_scale=- 1;     /* scaling factor to avoid floating point overflows */
+int   fold_constrained = 0; /* fold with constraints */
 int   do_backtrack=1;     /* calculate pair prob matrix in part_func() */
 int    noLonelyPairs = 0; /* avoid helices of length 1 */
 char backtrack_type='F';  /* 'C' require (1,N) to be bonded;
 			     'M' seq is part of s multi loop */
 
+char * option_string(void) {
+  static char options[100];
+  *options = '\0';
+  if (noGU) strcat(options, "-noGU ");
+  if (no_closingGU) strcat(options, "-noCloseGU ");
+  if (!tetra_loop) strcat(options, "-4 ");
+  if (noLonelyPairs) strcat(options, "-noLP ");
+  if (fold_constrained) strcat(options, "-C ");
+  if (dangles!=1) sprintf(options+strlen(options), "-d%d ", dangles);
+  if (temperature!=37.0) 
+    sprintf(options+strlen(options), "-T %f ", temperature);
+  return options;
+}
