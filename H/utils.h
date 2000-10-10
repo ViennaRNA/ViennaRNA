283f258e881c89f2c2968fd42b6424382fc00cf1
ivo <ivo>
2000-10-10 14:39:07 +0000

    annotation for lclint

--- H/utils.h
+++ H/utils.h
@@ -5,14 +5,17 @@
 #include "/usr/local/include/dmalloc.h"
 #define space(S) calloc(1,(S))
 #else
+/*@only@*/ /*@notnull@*/
 extern void  *space(unsigned size);           /* allocate space safely */
 #endif
+/*@exits@*/
 extern void   nrerror(const char message[]);  /* die with error message */
 extern void   init_rand(void);                /* make random number seeds */
 extern unsigned short xsubi[3];               /* current 48bit random number */
 extern double urn(void);                      /* random number from [0..1] */
 extern int    int_urn(int from, int to);      /* random integer */
 extern void   filecopy(FILE *from, FILE *to); /* inefficient `cp' */
+/*@observer@*/
 extern char  *time_stamp(void);               /* current date in a string */
 extern char  *random_string(int l, const char symbols[]);
 /* random string of length l using characters from symbols[] */
