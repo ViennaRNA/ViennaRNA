20de2fb960943f6cf5f6455b2270f55ed80b6ed8
ivo <ivo>
2000-10-17 09:26:39 +0000

    temperature is now double, LARGE_PF defined by default

--- H/fold_vars.h
+++ H/fold_vars.h
@@ -1,5 +1,5 @@
-/* to use doubles instead of floats in pf_fold() uncomment next line */
-/* #define LARGE_PF */
+/* to use floats instead of doubles in pf_fold() comment next line */
+#define LARGE_PF 
 #ifdef  LARGE_PF
 #define FLT_OR_DBL double
 #else
@@ -11,8 +11,9 @@ extern int  no_closingGU;   /* GU allowed only inside stacks */
 extern int  tetra_loop;     /* Fold with specially stable 4-loops */
 extern int  energy_set;     /* 0 = BP; 1=any mit GC; 2=any mit AU-parameter */
 extern int  dangles;	    /* use dangling end energies (not in part_func!) */
+/*@null@*/
 extern char *nonstandards;  /* contains allowed non standard bases */
-extern float temperature;   /* rescale parameters to this temperature */
+extern double temperature;   /* rescale parameters to this temperature */
 extern int  james_rule;     /* interior loops of size 2 get energy 0.8Kcal and
 			       no mismatches, default 1 */
 extern int  logML;          /* use logarithmic multiloop energy function */
@@ -26,9 +27,10 @@ extern struct bond  *base_pair; /* list of base pairs */
 
 extern FLT_OR_DBL *pr;          /* base pairing prob. matrix */
 extern int   *iindx;            /* pr[i,j] -> pr[iindx[i]-j] */
-extern float  pf_scale;         /* scaling factor to avoid float overflows*/
+extern double pf_scale;         /* scaling factor to avoid float overflows*/
 extern int    fold_constrained; /* fold with constraints */
 extern int    do_backtrack;     /* calculate pair prob matrix in part_func() */
 extern int    noLonelyPairs;    /* avoid helices of length 1 */
 extern char backtrack_type;     /* usually 'F'; 'C' require (1,N) to be bonded;
 				   'M' seq is part of a multi loop */
+char * option_string(void);
