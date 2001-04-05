2dfcadf73d11601894156d9d0df3d63634781a5e
ivo <ivo>
2001-04-05 07:33:42 +0000

    declare PS_rna_plot_a

--- H/PS_dot.h
+++ H/PS_dot.h
@@ -1,6 +1,8 @@
 /* routines from PS_dot.c */
 extern int PS_rna_plot(char *string, char *structure, char *file);
 /* write PostScript drawing of structure to file */
+extern int PS_rna_plot_a(char *string, char *structure, char *file, char **A);
+/* write PostScript drawing of structure to file with annotation */
 extern int gmlRNA(char *string, char *structure, char *ssfile, char option);
 /* structure drawing in gml */
 extern int ssv_rna_plot(char *string, char *structure, char *ssfile);
