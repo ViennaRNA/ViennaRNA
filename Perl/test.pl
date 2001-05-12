5ffd7012cd6ceabf10494a7435528e8664fca2a1
ivo <ivo>
2001-05-12 12:50:39 +0000

    updated to test.pl from ViennaRNA-1.4

--- Perl/test.pl
+++ Perl/test.pl
@@ -1,12 +1,12 @@
 # Before `make install' is performed this script should be runnable with
 # `make test'. After `make install' it should work as `perl test.pl'
+# Last changed Time-stamp: <2001-05-12 14:43:43 ivo>
 
 ######################### We start with some black magic to print on failure.
-
 # Change 1..1 below to 1..last_test_to_print .
 # (It may become useful if the test is moved to ./t subdirectory.)
 
-BEGIN { $| = 1; print "1..2\n"; }
+BEGIN { $| = 1; print "1..13\n"; }
 END {print "not ok 1\n" unless $loaded;}
 use RNA;
 $loaded = 1;
@@ -18,8 +18,9 @@ print "ok 1\n";
 # (correspondingly "not ok 13") depending on the success of chunk 13
 # of the test code):
 
-$seq1="CGCAGGGAUACCCGCG"; $seq2="GCGCCCAUAGGGACGC";
-$struc1=".((.(((...))))).";
+$seq1  ="CGCAGGGAUACCCGCG"; 
+$struc1="(((.((....)).)))";
+$seq2  ="GCGCCCAUAGGGACGC";
 $struc2="((((((...))).)))";
 # calculate a hamming distance (from util.c)
 if (RNA::hamming($seq1, $seq2) == 16) 
@@ -46,7 +47,7 @@ $T1 = RNA::make_tree($xstruc);
 $xstruc = RNA::expand_Full($struc2);
 $T2 = RNA::make_tree($xstruc);
 $tree_dist = RNA::tree_edit_distance($T1, $T2);  
-if ($tree_dist==6)
+if ($tree_dist==4)
     {print "ok 7\n"; } else { print "not ok 7\n"; }
 # check access to a C array
 if (RNA::ptrvalue($RNA::iindx,3)==108)
@@ -55,24 +56,41 @@ if (RNA::ptrvalue($RNA::iindx,3)==108)
 $p1 = RNA::get_pr(2,15);
 $ii = RNA::ptrvalue($RNA::iindx, 2);
 $p2 = RNA::ptrvalue($RNA::pr, $ii-15);
-if (($p1<0.976) && ($p1>0.975) && ($p1 == $p2))
+if (($p1<0.999) && ($p1>0.99) && (abs($p1-$p2)<1.2e-7))
     {print "ok 9\n"; } else { print "not ok 9 $p1 $p2\n" ;}
 
 $bpf = RNA::Make_bp_profile(length($seq1));
-$bpfi = RNA::deref_any($bpf, 2);
-if ((RNA::ptrvalue($bpfi, 0, "float")+RNA::ptrvalue($bpfi,1,'float')==1)&&
-    (RNA::ptrvalue($bpfi, 1, "float")>$p1))
-    {print "ok 10\n"; } else { print "not ok 10 $p1 $p2\n" ;}
+$bpfi = RNA::ptrcast(RNA::deref_any($bpf, 2), 'float *');
+if ((RNA::ptrvalue($bpfi, 0)+RNA::ptrvalue($bpfi,1)>.99999)&&
+    (RNA::ptrvalue($bpfi, 1)>=$p1)) {
+   print "ok 10\n"; }
+else { print "not ok 10 $p1 $p2 ",RNA::ptrvalue($bpfi, 1, "float"),"\n" ;}
 
 $pack = RNA::pack_structure($struc1);
-#print "$pack\n";
 if (RNA::unpack_structure($pack) eq $struc1) {
    print "ok 11\n";
 } else {
    print "not ok 11\n";
 }
+
+RNA::parse_structure($struc1);
+if (($RNA::loops==2) && ($RNA::pairs==5)&&($RNA::unpaired==6)&&
+    (RNA::ptrvalue($RNA::loop_degree,1)==2)) {
+   print "ok 12\n";
+} else {
+   print "not ok 12\n";
+}
+
 RNA::PS_rna_plot($seq1, $struc1, "test_ss.ps");
 RNA::PS_dot_plot($seq1, "test_dp.ps");
 RNA::ssv_rna_plot($seq1, $struct, "test.coord");
-print "$seq1, $struct, $mfe, $f\n";
+# print "$seq1, $struct, $mfe, $f\n";
 print "please check the two postscript files test_ss.ps and test_dp.ps\n";
+RNA::write_parameter_file("test.par");
+
+$RNA::symbolset = "GC";
+RNA::inverse_fold($seq1, $struc1);
+$ss = $seq1;
+RNA::fold($seq1, $ss);
+print 'not ' if ($ss ne $struc1);
+print "ok 13\n";
