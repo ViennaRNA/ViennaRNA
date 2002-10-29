9503763dc0d6624b7066e9f5237a773cfe7403e1
ivo <ivo>
2002-10-29 13:59:12 +0000

    *** empty log message ***

--- H/profiledist.h
+++ H/profiledist.h
@@ -1,14 +1,14 @@
 
-extern float  **Make_bp_profile(int length);
+extern float  (*Make_bp_profile(int length))[3];
 /* condense pair probability matrix pr into a vector containing probabilities
    for upstream paired, downstream paired and unpaired. This resulting
    probability profile is used as input for profile_edit_distance */
 
-extern float    profile_edit_distance(float **T1, float **T2);
+extern float    profile_edit_distance(float (*T1)[3], float (*T2)[3]);
 /* align two probability profiles */
 
-extern void     print_bppm(float **T);
+extern void     print_bppm(float (*T)[3]);
 /* print string representation of probability profile */
 
-extern void     free_profile(float **T);
+extern void     free_profile(float (*T)[3]);
 /* free space allocated in Make_bp_profile */
