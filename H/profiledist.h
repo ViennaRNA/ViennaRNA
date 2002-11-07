27c0cca56255f49380e7c77e57f79c38afd7bc57
ivo <ivo>
2002-11-07 11:49:55 +0000

    have Make_bp_profile simply return (float *) RNApdist -B works again

--- H/profiledist.h
+++ H/profiledist.h
@@ -1,14 +1,14 @@
 
-extern float  (*Make_bp_profile(int length))[3];
+extern float  *Make_bp_profile(int length);
 /* condense pair probability matrix pr into a vector containing probabilities
    for upstream paired, downstream paired and unpaired. This resulting
    probability profile is used as input for profile_edit_distance */
 
-extern float    profile_edit_distance(float (*T1)[3], float (*T2)[3]);
+extern float    profile_edit_distance(const float *T1, const float *T2);
 /* align two probability profiles */
 
-extern void     print_bppm(float (*T)[3]);
+extern void     print_bppm(const float *T);
 /* print string representation of probability profile */
 
-extern void     free_profile(float (*T)[3]);
+extern void     free_profile(float *T);
 /* free space allocated in Make_bp_profile */
--- lib/ProfileDist.c
+++ lib/ProfileDist.c
@@ -3,7 +3,7 @@
 		      Peter F Stadler, Ivo L Hofacker
 			    Vienna RNA Package
 */ 
-/* Last changed Time-stamp: <1998-06-28 17:12:50 ivo> */
+/* Last changed Time-stamp: <2002-11-07 12:46:16 ivo> */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,8 +14,8 @@
 #include  "fold_vars.h"
 #include  "part_func.h"
 #include  "utils.h"
-
-static char rcsid[] = "$Id: ProfileDist.c,v 1.5 2002/10/29 13:58:13 ivo Exp $";
+/*@unused@*/
+static char rcsid[] = "$Id: ProfileDist.c,v 1.6 2002/11/07 11:49:59 ivo Exp $";
 
 #define PUBLIC
 #define PRIVATE        static
@@ -25,26 +25,29 @@ static char rcsid[] = "$Id: ProfileDist.c,v 1.5 2002/10/29 13:58:13 ivo Exp $";
 
 PRIVATE int *alignment[2];
 
-PUBLIC float    profile_edit_distance(float (*T1)[3], float (*T2)[3]);
-PUBLIC float    (*Make_bp_profile(int length))[3]; 
-PUBLIC void     free_profile(float (*T)[3]);
-PRIVATE void    sprint_aligned_bppm(float (*T1)[3], float (*T2)[3]);
-PRIVATE float   PrfEditCost(int i, int j, float (*T1)[3], float (*T2)[3]);
-PRIVATE float   average(float x, float y);
+PUBLIC float    profile_edit_distance(const float *T1, const float *T2);
+PUBLIC float    *Make_bp_profile(int length); 
+PUBLIC void     free_profile(float *T);
+PRIVATE void    sprint_aligned_bppm(const float *T1, const float *T2);
+PRIVATE double  PrfEditCost(int i, int j, const float *T1, const float *T2);
+PRIVATE double  average(double x, double y);
 
 /*---------------------------------------------------------------------------*/
 
-PUBLIC float profile_edit_distance(float (*T1)[3], float (*T2)[3])
-
+PUBLIC float profile_edit_distance(const float *T1, const float *T2)
 {
+  /* align the 2 probability profiles T1, T2 */
+  /* This is like a Needleman-Wunsch alignment,
+     we should really use affine gap-costs ala Gotoh */
+
   float    **distance;
   short    **i_point, **j_point;
   
   int           i, j, i1, j1, pos, length1,length2;
   float         minus, plus, change, temp;
   
-  length1 = (int) T1[0][0];
-  length2 = (int) T2[0][0];
+  length1 = (int) T1[0];
+  length2 = (int) T2[0];
   distance = (float **)     space((length1 +1)*sizeof(float *));
   if(edit_backtrack){
     i_point  = (short **)  space((length1 +1)*sizeof(short *));
@@ -60,11 +63,11 @@ PUBLIC float profile_edit_distance(float (*T1)[3], float (*T2)[3])
   
   for(i = 1; i <= length1; i++) {
     distance[i][0] = distance[i-1][0]+PrfEditCost(i,0,T1,T2);
-    if(edit_backtrack){ i_point[i][0] = i-1; j_point[i][0] = 0;   }
+    if(edit_backtrack){ i_point[i][0] = (short) i-1; j_point[i][0] = 0;   }
   }
   for(j = 1; j <= length2; j++) {
     distance[0][j] = distance[0][j-1]+PrfEditCost(0,j,T1,T2);
-    if(edit_backtrack){ i_point[0][j] = 0;   j_point[0][j] = j-1; }
+    if(edit_backtrack){ i_point[0][j] = 0;   j_point[0][j] = (short) j-1; }
   }    
   for (i = 1; i <= length1; i++) {
     for (j = 1; j <= length2 ; j++) {
@@ -77,11 +80,11 @@ PUBLIC float profile_edit_distance(float (*T1)[3], float (*T2)[3])
       
       if(edit_backtrack){
 	if(distance[i][j] == change) {
-	  i_point[i][j]=i-1; j_point[i][j]=j-1;  }
+	  i_point[i][j]= (short)i-1; j_point[i][j]= (short) j-1;  }
 	else if(distance[i][j] == plus) {
-	  i_point[i][j]=i  ; j_point[i][j]=j-1;  }
+	  i_point[i][j]= (short)i  ; j_point[i][j]= (short)j-1;  }
 	else {
-	  i_point[i][j]=i-1; j_point[i][j]=j  ;  }
+	  i_point[i][j]= (short)i-1; j_point[i][j]= (short)j  ;  }
       }
     } 
     /* printf("\n"); */
@@ -139,32 +142,32 @@ PUBLIC float profile_edit_distance(float (*T1)[3], float (*T2)[3])
 
 /*---------------------------------------------------------------------------*/
 
-PRIVATE float PrfEditCost(int i, int j, float (*T1)[3], float (*T2)[3])
+PRIVATE double PrfEditCost(int i, int j, const float *T1, const float *T2)
 {
-  float  dist;
-  int    k,kmax;
+  double  dist;
+  int    k, kmax;
+
+  kmax = (int) T1[1];
+  if ((int) T2[1] != kmax) nrerror("inconsistent Profiles in PrfEditCost");
   
   if(i==0) {
-    kmax = T2[0][1];
     for(dist = 0. ,k=0 ; k<kmax ; k++) 
-      dist += T2[j][k];
+      dist += T2[j*kmax+k];
   }
   if(j==0) {
-    kmax = T1[0][1];
     for(dist = 0. ,k=0 ; k<kmax ; k++) 
-      dist += T1[i][k];
+      dist += T1[i*kmax+k];
   }
   if((i>0)&&(j>0)) {
-    kmax = MIN(T1[0][1],T2[0][1]);
     for(dist = 2.,k=0; k<kmax; k++)
-      dist -= 2.*average(T1[i][k],T2[j][k]);
+      dist -= 2.*average(T1[i*kmax+k],T2[j*kmax+k]);
   }
   return dist;
 }
 
 /*---------------------------------------------------------------------------*/
 
-PRIVATE float average(float x, float y)
+PRIVATE double average(double x, double y)
 
 /* can be essentially anything that fulfils :
    1.)     a(x,y)  =  a(y,x)
@@ -172,7 +175,7 @@ PRIVATE float average(float x, float y)
    3.)     a(x,y) <=  (x+y)/2
    4.)     a(x,x) >=  a(x,y)  for 0<= x,y <= 1
    As in Bonhoeffer et al (1993) 'RNA Multi Structure Landscapes',
-   Eur. Biophys. J. {in press} we have chosen  the geometric mean.
+   Eur. Biophys. J. 22: 13-24 we have chosen  the geometric mean.
 */ 
 
 {
@@ -183,29 +186,30 @@ PRIVATE float average(float x, float y)
 
 /*---------------------------------------------------------------------------*/
 
-PUBLIC float (*Make_bp_profile(int length))[3]
+PUBLIC float *Make_bp_profile(int length)
 {
    int i,j;
    int L=3;
-   float (*P)[3]; /* P[][0] unpaired, P[][1] upstream p, P[][2] downstream p */
-
-   P =  space((length+1)*3*sizeof(float *));
-   P[0][0] = (float) length;
-   P[0][1] = (float) L;
+   float *P; /* P[i*3+0] unpaired, P[i*3+1] upstream, P[i*3+2] downstream p */
+   
+   P =  (float *) space((length+1)*3*sizeof(float));
+   /* indices start at 1 use first entries to store length and dimension */
+   P[0] = (float) length;
+   P[1] = (float) L;
 
    for( i=1; i<length; i++)
      for( j=i+1; j<=length; j++ ) {
-       P[i][1] += pr[iindx[i]-j];
-       P[j][2] += pr[iindx[i]-j];
+       P[i*L+1] += pr[iindx[i]-j];
+       P[j*L+2] += pr[iindx[i]-j];
      } 
    for( i=1; i<=length; i++)
-     P[i][0] = 1 - P[i][1] - P[i][2];
-   return P;
+     P[i*3+0] = 1 - P[i*3+1] - P[i*3+2];
+   return (float *) P;
 }
  
 /*---------------------------------------------------------------------------*/
          
-PRIVATE void sprint_aligned_bppm(float (*T1)[3], float (*T2)[3])
+PRIVATE void sprint_aligned_bppm(const float *T1, const float *T2)
 {
    int     i, length; 
    length = alignment[0][0];
@@ -213,27 +217,26 @@ PRIVATE void sprint_aligned_bppm(float (*T1)[3], float (*T2)[3])
    aligned_line[1] = (char *) space((length+1)*sizeof(char));
    for(i=1; i<=length; i++){
       if(alignment[0][i] ==0) aligned_line[0][i-1] = '_';
-      else { aligned_line[0][i-1] = bppm_symbol(T1[alignment[0][i]]); }
+      else { aligned_line[0][i-1] = bppm_symbol(T1+alignment[0][i]*3); }
       if(alignment[1][i] ==0) aligned_line[1][i-1] = '_';
-      else { aligned_line[1][i-1] = bppm_symbol(T2[alignment[1][i]]); }
+      else { aligned_line[1][i-1] = bppm_symbol(T2+alignment[1][i]*3); }
    }
 }
 
 /*---------------------------------------------------------------------------*/
 
-PUBLIC void print_bppm(float (*T)[3])
+PUBLIC void print_bppm(const float *T)
 {
    int i;
-   for(i=1; i<=( (int)T[0][0]); i++)
-      printf("%c",bppm_symbol(T[i]));
+   for(i=1; i<=( (int)T[0]); i++)
+      printf("%c",bppm_symbol(T+i*3));
    printf("\n");
 }
 
 /*---------------------------------------------------------------------------*/
 
-PUBLIC void     free_profile(float (*T)[3])
+PUBLIC void     free_profile(float *T)
 {
-   int i, length;
    free(T);
 }
    
