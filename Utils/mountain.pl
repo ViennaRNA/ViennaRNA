ce09307b465b1de718b00ede7050cbf9784048ae
ivo <ivo>
2003-07-18 07:52:05 +0000

    use stric; use warnings

--- Utils/mountain.pl
+++ Utils/mountain.pl
@@ -1,6 +1,6 @@
-#!/usr/local/bin/perl
+#!/usr/bin/perl -w
 # -*-Perl-*-
-# Last changed Time-stamp: <1998-04-05 19:57:42 ivo>
+# Last changed Time-stamp: <2003-07-18 09:50:16 ivo>
 # produce Pauline Hogeweg's mountain representation *_dp.ps files
 # writes 3 sets of x y data separated by a "&"
 # the first two sets are mountain representations from base pair probabilities
@@ -9,14 +9,15 @@
 # third set a measure of well-definedness: the entropy of the pair probs of
 # base i, sp[i] = -Sum p_i * ln(p_i). Well-defined regions have low entropy.
 #
-# use e.g. as  mountain.pl dot.ps | xmgr -pipe
+# use e.g. as  mountain.pl dot.ps | xmgrace -pipe
 
-#my( @mm, @mp, @sp, @p0, $i, $max, $length);  # perl5 only
+use strict;
+our (@mm, @mp, @pp, @sp, @p0, $i, $max, $length);  # perl5 only
 
-$sep = "&";   # xmgr uses & to separate data sets  
+my $sep = "&";   # xmgr uses & to separate data sets  
 
 while (<>) {
-#    my ($seq,$i,$j,$p,$id);
+    my ($seq,$i,$j,$p,$id);
     chop;
     if (/\/sequence \{ \((\S*)[\\\)]/) {  
         $seq = $1;              # empty for new version
@@ -28,13 +29,14 @@ while (<>) {
 	$length = length($seq);
 	next;
     }
-    
+
+    next unless /box$/;    
     ($i, $j, $p, $id) = split;
     if ($id eq "ubox") {
 	$p *= $p;           # square it to probability
 	$mp[$i+1] += $p; 
 	$mp[$j]   -= $p;
-	$ss = $p*log($p);
+	my $ss = $p*log($p);
 	$sp[$i] += $ss;
 	$sp[$j] += $ss;
 	$pp[$i] += $p;
@@ -45,12 +47,14 @@ while (<>) {
 	$mm[$j]--;
     }
 }
+$mp[0] = $mm[0] = $max = 0;
 for ($i=1; $i<=$length; $i++) {
+    no warnings;
     $mp[$i]+=$mp[$i-1];
     $max = $mp[$i] if ($mp[$i]>$max);
     $mm[$i]+=$mm[$i-1];
     $max = $mp[$i] if ($mp[$i]>$max);
-    $sp[$i] += (1-$pp[$i])*log(1-$pp[$i])
+    $sp[$i] += (1-$pp[$i])*log(1-$pp[$i]);
 }
 
 # print the results for plotting
@@ -63,7 +67,7 @@ for ($i=1; $i<=$length; $i++) {
     printf("%4d  %4d\n", $i, $mm[$i]);
 }	
 print "&\n";
-$log2 = log(2);
+my $log2 = log(2);
 for ($i=1; $i<=$length; $i++) {
     printf("%4d  %7.5g\n", $i, -$sp[$i]/$log2);
 }	
