aeb1e74b0100539317743e4082986b149a45c77a
ivo <ivo>
2006-01-16 11:38:40 +0000

    *** empty log message ***

--- H/part_func_co.h
+++ H/part_func_co.h
@@ -1,18 +1,19 @@
 /* functions from part_func.c */
 /* calculate partition function and base pair probabilities */
-#define LARGE_PF 
+#define LARGE_PF
 #ifdef  LARGE_PF
 #define FLT_OR_DBL double
 #else
 #define FLT_OR_DBL float
 #endif
+extern int mirnatog; /*toggles no intrabp in 2nd mol*/
 
-extern float  co_pf_fold(char *sequence, char *structure);/* calculate partition function and base pair probabilities */
+extern float  co_pf_fold(char *sequence, char *structure); /* calculate partition function and base pair probabilities */
 extern void   init_co_pf_fold(int length);
-extern void   free_co_pf_arrays(void); 
+extern void   free_co_pf_arrays(void);
 extern void   update_co_pf_params(int length); /*recalculate energy parameters */
 extern char   co_bppm_symbol(float *x);    /* string representation of structure */
-extern  void compute_probabilities(double *FEAB,double *FEAA,
+extern void   compute_probabilities(double *FEAB,double *FEAA,
 				     double *FEBB,double *FEA,
 				     double *FEB, struct plist  *prAB,
 				     struct plist  *prAA, struct plist  *prBB,
@@ -29,7 +30,7 @@ typedef struct ConcEnt {
   double BBc;
   double Ac;
   double Bc;
-}ConcEnt;
+} ConcEnt;
 
 
 
