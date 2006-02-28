943b71621415f3aabadbab1695641c68885cb8e0
ivo <ivo>
2006-02-28 15:05:34 +0000

    add energy evaluation for circular RNAs add backtrack_fold_from_pair() to backtrack partial structures

--- H/fold.h
+++ H/fold.h
@@ -1,8 +1,10 @@
 /* function from fold.c */
-extern float  fold(const char *sequence, char *structure); 
+extern float  fold(const char *sequence, char *structure);
 /* calculate mfe-structure of sequence */
 extern float  energy_of_struct(const char *string, const char *structure);
 /* calculate energy of string on structure */
 extern void   free_arrays(void);           /* free arrays for mfe folding */
 extern void   initialize_fold(int length); /* allocate arrays for folding */
 extern void   update_fold_params(void);    /* recalculate parameters */
+extern char  *backtrack_fold_from_pair(char *sequence, int i, int j);
+extern float energy_of_circ_struct(const char *string, const char *structure);
