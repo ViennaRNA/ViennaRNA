a3778b200f8dbb6efb45ebd8c52765b240a791e2
ivo <ivo>
2006-07-14 12:47:06 +0000

    undo uli's commit

--- H/fold_vars.h
+++ H/fold_vars.h
@@ -2,10 +2,8 @@
 #define LARGE_PF 
 #ifdef  LARGE_PF
 #define FLT_OR_DBL double
-#define EQUAL(A,B) (fabs((A)-(B)) < 100*DBL_EPSILON)
 #else
 #define FLT_OR_DBL float
-#define EQUAL(A,B) (fabs((A)-(B)) < 100*FLT_EPSILON)
 #endif
 
 extern int  noGU;           /* GU not allowed at all */
@@ -20,11 +18,7 @@ extern int  james_rule;     /* interior loops of size 2 get energy 0.8Kcal and
 			       no mismatches, default 1 */
 extern int  logML;          /* use logarithmic multiloop energy function */
 extern int  cut_point;      /* first position of 2nd strand for co-folding */
-/* extern int  w;           length of region of interaction */
-extern int  unstr;          /* length of unpaired region (pr_unpaired output)*/
-extern int  upmode;         /* output mode for pf_unpaired and pf_up()*/
-/* int incr3;               add x unpaired bases after 3'end of short RNA*/
-/* int incr5;               add x unpaired bases after 5'end of short RNA*/
+
 typedef struct bond {               /* base pair */
    int i;
    int j;
@@ -34,7 +28,6 @@ extern bondT  *base_pair; /* list of base pairs */
 extern FLT_OR_DBL *pr;          /* base pairing prob. matrix */
 extern int   *iindx;            /* pr[i,j] -> pr[iindx[i]-j] */
 extern double pf_scale;         /* scaling factor to avoid float overflows*/
-extern double s_fact;           /* user defined contribution to pf_scale */
 extern int    fold_constrained; /* fold with constraints */
 extern int    do_backtrack;     /* calculate pair prob matrix in part_func() */
 extern int    noLonelyPairs;    /* avoid helices of length 1 */
