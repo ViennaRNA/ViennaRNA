0456f2f605a8b403e05c7f8c496438ad8dee52cc
ivo <ivo>
2007-07-04 08:19:34 +0000

    *** empty log message ***

--- H/fold.h
+++ H/fold.h
@@ -8,3 +8,4 @@ extern void   initialize_fold(int length); /* allocate arrays for folding */
 extern void   update_fold_params(void);    /* recalculate parameters */
 extern char  *backtrack_fold_from_pair(char *sequence, int i, int j);
 extern float energy_of_circ_struct(const char *string, const char *structure);
+extern int loop_energy(short * ptable, short *s, short *s1, int i);
