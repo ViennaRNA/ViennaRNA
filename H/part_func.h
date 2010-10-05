07eddbbed44ecb17b806be404f71863eebdaae9f
Ronny Lorenz <ronny@knowledge.(none)>
2010-10-05 09:33:54 +0200

    removed a doxygen < 1.7 bug in the documentation...

--- H/part_func.h
+++ H/part_func.h
@@ -106,7 +106,9 @@ void    update_pf_params(int length);
 ***
 *** Accessing the base pair probabilities for a pair (i,j) is achieved by
 *** \verbatim FLT_OR_DBL *pr = export_bppm(); pr_ij = pr[iindx[i]-j]; \endverbatim
-*** 
+***
+*** \see get_iindx()
+*** \return A pointer to the base pair probability array 
 **/
 FLT_OR_DBL *export_bppm(void);
 
@@ -155,7 +157,7 @@ int     get_pf_arrays(short **S_p, short **S1_p, char **ptype_p, FLT_OR_DBL **qb
 *** This function is a threadsafe replacement for \ref centroid() with a 'plist' input
 ***
 *** The centroid is the structure with the minimal average distance to all other structures
-*** \n\f$ <d(S)> = \sum_{(i,j) \in S} (1-p_{ij}) + \sum_{(i,j) \notin S} p_{ij} \f$\n
+*** \n \f$ <d(S)> = \sum_{(i,j) \in S} (1-p_{ij}) + \sum_{(i,j) \notin S} p_{ij} \f$ \n
 *** Thus, the centroid is simply the structure containing all pairs with \f$p_ij>0.5\f$
 *** The distance of the centroid to the ensemble is written to the memory adressed by \a dist.
 ***
@@ -172,7 +174,7 @@ char    *get_centroid_struct_pl(int length, double *dist, plist *pl);
 *** This function is a threadsafe replacement for \ref centroid() with a probability array input
 ***
 *** The centroid is the structure with the minimal average distance to all other structures
-*** \n\f$ <d(S)> = \sum_{(i,j) \in S} (1-p_{ij}) + \sum_{(i,j) \notin S} p_{ij} \f$\n
+*** \n \f$ <d(S)> = \sum_{(i,j) \in S} (1-p_{ij}) + \sum_{(i,j) \notin S} p_{ij} \f$ \n
 *** Thus, the centroid is simply the structure containing all pairs with \f$p_ij>0.5\f$
 *** The distance of the centroid to the ensemble is written to the memory adressed by \a dist.
 ***
