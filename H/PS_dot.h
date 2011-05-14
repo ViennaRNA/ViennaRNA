3f0bacb8f368290cc06e40fa21a844b6ace18607
Ronny Lorenz <ronny@tbi.univie.ac.at>
2011-05-14 12:48:55 +0200

    some cleanup due to rna structure plotting layout algorithms. all definitions now moved to plot_layouts.c. The declarations are included in PS_dot.h to provide backward compatibility

--- H/PS_dot.h
+++ H/PS_dot.h
@@ -2,6 +2,7 @@
 #define __VIENNA_RNA_PACKAGE_PS_DOT_H__
 
 #include "data_structures.h"
+#include "plot_layouts.h"
 
 #ifdef __GNUC__
 #define DEPRECATED(func) func __attribute__ ((deprecated))
@@ -10,52 +11,10 @@
 #endif
 
 /**
- *  \brief Definition of Plot type <i>simple</i>
- *
- *  This is the plot type definition for several RNA structure plotting functions telling
- *  them to use <b>Simple</b> plotting algorithm
- *
- *  \see rna_plot_type, PS_rna_plot_a(), PS_rna_plot(), svg_rna_plot(), gmlRNA(), ssv_rna_plot(), xrna_plot()
- */
-#define VRNA_PLOT_TYPE_SIMPLE     0
-
-/**
- *  \brief Definition of Plot type <i>Naview</i>
- *
- *  This is the plot type definition for several RNA structure plotting functions telling
- *  them to use <b>Naview</b> plotting algorithm
- *
- *  \see rna_plot_type, PS_rna_plot_a(), PS_rna_plot(), svg_rna_plot(), gmlRNA(), ssv_rna_plot(), xrna_plot()
- */
-#define VRNA_PLOT_TYPE_NAVIEW     1
-
-/**
- *  \brief Definition of Plot type <i>Circular</i>
- *
- *  This is the plot type definition for several RNA structure plotting functions telling
- *  them to produce a <b>Circular plot</b>
- *
- *  \see rna_plot_type, PS_rna_plot_a(), PS_rna_plot(), svg_rna_plot(), gmlRNA(), ssv_rna_plot(), xrna_plot()
- */
-#define VRNA_PLOT_TYPE_CIRCULAR   2
-
-
-/**
  *  \file PS_dot.h
  *  \brief Various functions for plotting RNA secondary structures, dot-plots and other
  *  visualizations
  */
-
-/**
- *  \brief Switch for changing the secondary structure layout algorithm
- *
- *  Current possibility are 0 for a simple radial drawing or 1 for the modified
- *  radial drawing taken from the \e naview program of \ref bruccoleri_88 "Bruccoleri & Heinrich (1988)".
- *
- *  \see #VRNA_PLOT_TYPE_SIMPLE, #VRNA_PLOT_TYPE_NAVIEW, #VRNA_PLOT_TYPE_CIRCULAR
- *
- */
-extern int rna_plot_type;
 
 /**
  *  \brief Produce a secondary structure graph in PostScript and write it to 'filename'.
@@ -167,39 +126,6 @@ int PS_dot_plot_turn(char *seq, struct plist *pl, char *filename, int winSize);
 
 int PS_color_aln(const char *structure, const char *filename, const char *seqs[], const char *names[]);
 
-
-/**
- *  \brief Calculate nucleotide coordinates for secondary structure plot the <i>Simple way</i>
- *
- *  \see make_pair_table(), rna_plot_type, simple_circplot_coordinates(), naview_xy_coordinates(), PS_rna_plot_a(),
- *  PS_rna_plot, svg_rna_plot()
- *
- *  \param  pair_table  The pair table of the secondary structure
- *  \param  x           a pointer to an array with enough allocated space to hold the x coordinates
- *  \param  y           a pointer to an array with enough allocated space to hold the y coordinates
- *  \return             length of sequence on success, 0 otherwise
- */
-int simple_xy_coordinates(short *pair_table, float *X, float *Y);
-
-/**
- *  \brief Calculate nucleotide coordinates for <i>Circular Plot</i>
- *
- *  This function calculates the coordinates of nucleotides mapped in equal distancies onto a unit circle.
- *  Additional to the actual R<sup>2</sup> coordinates the function provides a radius scaling factor
- *  for all nucleotides that are involved in a base pairing. This scaling factor may be used for
- *  computing coordinates for a second tangential point \f$P^t\f$ needed to draw quadratic bezier curves.
- *  E.g. \f$ P^{t}_x[i] = X[i] * R[i]\f$ and \f$P^{t}_y[i] = Y[i] * R[i]\f$
- *
- *  \see make_pair_table(), rna_plot_type, simple_xy_coordinates(), naview_xy_coordinates(), PS_rna_plot_a(),
- *  PS_rna_plot, svg_rna_plot()
- *
- *  \param  pair_table  The pair table of the secondary structure
- *  \param  x           a pointer to an array with enough allocated space to hold the x coordinates
- *  \param  y           a pointer to an array with enough allocated space to hold the y coordinates
- *  \param  r           a pointer to an array with enough allocated space to hold the scaling factors
- *  \return             length of sequence on success, 0 otherwise
- */
-int simple_circplot_coordinates(short *pair_table, float *x, float *y, float *r);  
 
 /**
  *  Wrapper to PS_dot_plot_list
--- lib/PS_dot.c
+++ lib/PS_dot.c
@@ -13,15 +13,10 @@
 #include <ctype.h>
 #include "utils.h"
 #include "fold_vars.h"
-#include "naview.h"
 #include "PS_dot.h"
 #include "pair_mat.h"
 #include "aln_util.h"
-
-#ifdef _OPENMP
-#include <omp.h>
-#endif
-
+#include "plot_layouts.h"
 
 static char UNUSED rcsid[] = "$Id: PS_dot.c,v 1.38 2007/02/02 15:18:13 ivo Exp $";
 
@@ -32,22 +27,10 @@ static char UNUSED rcsid[] = "$Id: PS_dot.c,v 1.38 2007/02/02 15:18:13 ivo Exp $
 #define SIZE 452.
 #define PMIN 0.00001
 
-PUBLIC int   rna_plot_type = 1;  /* 0 = simple, 1 = naview */
-
 /* local functions */
-PRIVATE void   loop(int i, int j, short *pair_table);
 PRIVATE FILE  *PS_dot_common(char *seq, char *wastlfile, char *comment,
                              int winsize);
 
-/* local variables for parsing routines */
-PRIVATE float  *angle;
-PRIVATE int    *loop_size, *stack_size;
-PRIVATE int     lp, stk;
-
-#ifdef _OPENMP
-/* NOTE: all threadprivate variables are uninitialized when entering a thread! */
-#pragma omp threadprivate(angle, loop_size, stack_size, lp, stk)
-#endif
 
 /*---------------------------------------------------------------------------*/
 
@@ -891,137 +874,6 @@ int PS_color_dot_plot(char *seq, cpair *pi, char *wastlfile) {
 
 /*---------------------------------------------------------------------------*/
 
-PUBLIC int simple_xy_coordinates(short *pair_table, float *x, float *y)
-{
-  float INIT_ANGLE=0.;     /* initial bending angle */
-  float INIT_X = 100.;     /* coordinate of first digit */
-  float INIT_Y = 100.;     /* see above */
-  float RADIUS =  15.;
-
-  int i, length;
-  float  alpha;
-
-  length = pair_table[0];
-  angle =      (float*) space( (length+5)*sizeof(float) );
-  loop_size  =   (int*) space( 16+(length/5)*sizeof(int) );
-  stack_size =   (int*) space( 16+(length/5)*sizeof(int) );
-  lp = stk = 0;
-  loop(0, length+1, pair_table);
-  loop_size[lp] -= 2;     /* correct for cheating with function loop */
-
-  alpha = INIT_ANGLE;
-  x[0]  = INIT_X;
-  y[0]  = INIT_Y;
-
-  for (i = 1; i <= length; i++) {
-    x[i] = x[i-1]+RADIUS*cos(alpha);
-    y[i] = y[i-1]+RADIUS*sin(alpha);
-    alpha += PI-angle[i+1];
-  }
-  free(angle);
-  free(loop_size);
-  free(stack_size);
-
-  return length;
-
-}
-
-PUBLIC int simple_circplot_coordinates(short *pair_table, float *x, float *y, float *r){
-  unsigned int  length = pair_table[0];
-  unsigned int  i;
-  float         d = 2*PI/length;
-  float         dr = 0;
-  for(i=0; i < length; i++){
-    x[i] = cos(i * d - PI/2);
-    y[i] = sin(i * d - PI/2);
-    if(i<pair_table[i+1]){
-      dr = (pair_table[i+1]-i+1 <= (length/2 + 1)) ? pair_table[i+1]-i : i + length - pair_table[i+1];
-      r[i] = 1. - (2.*dr/(float)length);
-    }
-    else if(pair_table[i+1]){
-      r[i] = r[pair_table[i+1]-1];
-    }
-    else{
-      r[i] = -1.0;
-    }
-  }
-  return length;
-}
-/*---------------------------------------------------------------------------*/
-
-PRIVATE void loop(int i, int j, short *pair_table)
-             /* i, j are the positions AFTER the last pair of a stack; i.e
-                i-1 and j+1 are paired. */
-{
-  int    count = 2;   /* counts the VERTICES of a loop polygon; that's
-                           NOT necessarily the number of unpaired bases!
-                           Upon entry the loop has already 2 vertices, namely
-                           the pair i-1/j+1.  */
-
-  int    r = 0, bubble = 0; /* bubble counts the unpaired digits in loops */
-
-  int    i_old, partner, k, l, start_k, start_l, fill, ladder;
-  int    begin, v, diff;
-  float  polygon;
-
-  short *remember;
-
-  remember = (short *) space((1+(j-i)/5)*2*sizeof(short));
-
-  i_old = i-1, j++;         /* j has now been set to the partner of the
-                               previous pair for correct while-loop
-                               termination.  */
-  while (i != j) {
-    partner = pair_table[i];
-    if ((!partner) || (i==0))
-      i++, count++, bubble++;
-    else {
-      count += 2;
-      k = i, l = partner;    /* beginning of stack */
-      remember[++r] = k;
-      remember[++r] = l;
-      i = partner+1;         /* next i for the current loop */
-
-      start_k = k, start_l = l;
-      ladder = 0;
-      do {
-        k++, l--, ladder++;        /* go along the stack region */
-      }
-      while (pair_table[k] == l);
-
-      fill = ladder-2;
-      if (ladder >= 2) {
-        angle[start_k+1+fill] += PIHALF;   /*  Loop entries and    */
-        angle[start_l-1-fill] += PIHALF;   /*  exits get an        */
-        angle[start_k]        += PIHALF;   /*  additional PI/2.    */
-        angle[start_l]        += PIHALF;   /*  Why ? (exercise)    */
-        if (ladder > 2) {
-          for (; fill >= 1; fill--) {
-            angle[start_k+fill] = PI;    /*  fill in the angles  */
-            angle[start_l-fill] = PI;    /*  for the backbone    */
-          }
-        }
-      }
-      stack_size[++stk] = ladder;
-      loop(k, l, pair_table);
-    }
-  }
-  polygon = PI*(count-2)/(float)count; /* bending angle in loop polygon */
-  remember[++r] = j;
-  begin = i_old < 0 ? 0 : i_old;
-  for (v = 1; v <= r; v++) {
-    diff  = remember[v]-begin;
-    for (fill = 0; fill <= diff; fill++)
-      angle[begin+fill] += polygon;
-    if (v > r)
-      break;
-    begin = remember[++v];
-  }
-  loop_size[++lp] = bubble;
-  free(remember);
-}
-
-/*---------------------------------------------------------------------------*/
 
 
 PUBLIC int PS_dot_plot_list(char *seq, char *wastlfile, plist *pl, plist *mf, char *comment) {
