25c1b65dd76a1d6ceecaef7ca7fb5d9d44c366ca
Ronny Lorenz <ronny@tbi.univie.ac.at>
2013-03-11 12:57:55 +0100

    - repaired making of the Python interface - changed some implementation details on deprecated functions in params.c

--- src/ViennaRNA/params.c
+++ src/ViennaRNA/params.c
@@ -561,11 +561,7 @@ PUBLIC paramT *get_parameter_copy(paramT *par){
 /*###########################################*/
 
 PUBLIC paramT *copy_parameters(void){
-  paramT *copy;
-  if (p.id != id) scale_parameters();
-  copy = (paramT *) space(sizeof(paramT));
-  memcpy(copy, &p, sizeof(paramT));
-  return copy;
+  return scale_parameters();
 }
 
 PUBLIC paramT *set_parameters(paramT *dest){
@@ -574,11 +570,7 @@ PUBLIC paramT *set_parameters(paramT *dest){
 }
 
 PUBLIC pf_paramT *copy_pf_param(void){
-  pf_paramT *copy;
-  if (pf.id != pf_id) scale_pf_parameters();
-  copy = (pf_paramT *) space(sizeof(pf_paramT));
-  memcpy(copy, &pf, sizeof(pf_paramT));
-  return copy;
+  return get_scaled_pf_parameters();
 }
 
 PUBLIC pf_paramT *set_pf_param(paramT *dest){
@@ -588,164 +580,4 @@ PUBLIC pf_paramT *set_pf_param(paramT *dest){
 
 PUBLIC pf_paramT *scale_pf_parameters(void){
   return get_scaled_pf_parameters();
-#if 0
-  /* scale energy parameters and pre-calculate Boltzmann weights */
-  unsigned int i, j, k, l;
-  double  kT, TT;
-  double  GT;
-
-  /* scale pf_params() in partfunc.c is only a wrapper, that calls
-     this functions !! */
-
-  pf.temperature = temperature;
-  kT = (pf.temperature+K0)*GASCONST;   /* kT in cal/mol  */
-  TT = (pf.temperature+K0)/(Tmeasure);
-
-   /* loop energies: hairpins, bulges, interior, mulit-loops */
-  for (i=0; i<31; i++) {
-    GT =  hairpin37[i]*TT;
-    pf.exphairpin[i] = exp( -GT*10./kT);
-  }
-  for (i=0; i<=MIN2(30, MAXLOOP); i++) {
-    GT =  bulge37[i]*TT;
-    pf.expbulge[i] = exp( -GT*10./kT);
-    GT =  internal_loop37[i]*TT;
-    pf.expinternal[i] = exp( -GT*10./kT);
-  }
-  /* special case of size 2 interior loops (single mismatch) */
-  if (james_rule) pf.expinternal[2] = exp ( -80*10./kT);
-
-  pf.lxc = lxc37*TT;
-
-  GT =  DuplexInitdH - (DuplexInitdH - DuplexInit37)*TT;
-  pf.expDuplexInit = exp( -GT*10./kT);
-
-  for (i=31; i<=MAXLOOP; i++) {
-    GT = bulge37[30]*TT + (pf.lxc*log( i/30.));
-    pf.expbulge[i] = exp( -GT*10./kT);
-    GT = internal_loop37[30]*TT + (pf.lxc*log( i/30.));
-    pf.expinternal[i] = exp( -GT*10./kT);
-  }
-
-  GT = niniodH - (niniodH - ninio37)*TT;
-  for (j=0; j<=MAXLOOP; j++)
-      pf.expninio[2][j]=exp(-MIN2(MAX_NINIO,j*GT)*10./kT);
-
-  for (i=0; (i*7)<strlen(Tetraloops); i++) {
-    GT = TetraloopdH[i] - (TetraloopdH[i]-Tetraloop37[i])*TT;
-    pf.exptetra[i] = exp( -GT*10./kT);
-  }
-  for (i=0; (i*5)<strlen(Triloops); i++) {
-    GT = TriloopdH[i] - (TriloopdH[i]-Triloop37[i])*TT;
-    pf.exptri[i] = exp( -GT*10./kT);
-  }
-  for (i=0; (i*9)<strlen(Hexaloops); i++) {
-    GT = HexaloopdH[i] - (HexaloopdH[i]-Hexaloop37[i])*TT;
-    pf.exphex[i] = exp( -GT*10./kT);
-  }
-  GT =  ML_closing37*TT;
-  pf.expMLclosing = exp( -GT*10./kT);
-
-  for (i=0; i<=NBPAIRS; i++) { /* includes AU penalty */
-    GT =  ML_intern37*TT;
-    /* if (i>2) GT += TerminalAU; */
-    pf.expMLintern[i] = exp( -GT*10./kT);
-  }
-  GT = TerminalAUdH - (TerminalAUdH - TerminalAU37)*TT;
-  pf.expTermAU = exp(-GT*10./kT);
-
-  GT = ML_BASE37*TT;
-  pf.expMLbase=exp(-10.*GT/kT);
-
-
-  /* if dangle_model==0 just set their energy to 0,
-     don't let dangle energies become > 0 (at large temps),
-     but make sure go smoothly to 0                        */
-  for (i=0; i<=NBPAIRS; i++)
-    for (j=0; j<=4; j++) {
-      if (dangles) {
-        GT = dangle5_dH[i][j] - (dangle5_dH[i][j] - dangle5_37[i][j])*TT;
-        pf.expdangle5[i][j] = exp(SMOOTH(-GT)*10./kT);
-        GT = dangle3_dH[i][j] - (dangle3_dH[i][j] - dangle3_37[i][j])*TT;
-        pf.expdangle3[i][j] =  exp(SMOOTH(-GT)*10./kT);
-      } else
-        pf.expdangle3[i][j] = pf.expdangle5[i][j] = 1;
-      if (i>2) /* add TermAU penalty into dangle3 */
-        pf.expdangle3[i][j] *= pf.expTermAU;
-    }
-
-  /* stacking energies */
-  for (i=0; i<=NBPAIRS; i++)
-    for (j=0; j<=NBPAIRS; j++) {
-      GT =  stackdH[i][j] - (stackdH[i][j] - stack37[i][j])*TT;
-      pf.expstack[i][j] = exp( -GT*10./kT);
-    }
-
-  /* mismatch energies */
-  for (i=0; i<=NBPAIRS; i++)
-    for (j=0; j<5; j++)
-      for (k=0; k<5; k++) {
-        GT =  mismatchIdH[i][j][k] - ( mismatchIdH[i][j][k] - mismatchI37[i][j][k])*TT;
-        pf.expmismatchI[i][j][k] = exp(-GT*10./kT);
-        GT = mismatch1nIdH[i][j][k] - (mismatch1nIdH[i][j][k] - mismatch1nI37[i][j][k])*TT;
-        pf.expmismatch1nI[i][j][k] = exp(-GT*10./kT);
-        GT = mismatchHdH[i][j][k] - (mismatchHdH[i][j][k] - mismatchH37[i][j][k])*TT;
-        pf.expmismatchH[i][j][k] = exp(-GT*10./kT);
-        GT = mismatch23IdH[i][j][k] - (mismatch23IdH[i][j][k] - mismatch23I37[i][j][k])*TT;
-        pf.expmismatch23I[i][j][k] = exp(-GT*10./kT);
-        if (dangles) {
-          GT = mismatchMdH[i][j][k] - (mismatchMdH[i][j][k] - mismatchM37[i][j][k])*TT;
-          pf.expmismatchM[i][j][k] = exp(-GT*10./kT);
-          GT =  mismatchExtdH[i][j][k] - ( mismatchExtdH[i][j][k] - mismatchExt37[i][j][k])*TT;
-          pf.expmismatchExt[i][j][k] = exp(-GT*10./kT);
-        }
-        else{
-          pf.expmismatchM[i][j][k] = pf.expmismatchExt[i][j][k] = 1.;
-        }
-      }
-
-
-  /* interior lops of length 2 */
-  for (i=0; i<=NBPAIRS; i++)
-    for (j=0; j<=NBPAIRS; j++)
-      for (k=0; k<5; k++)
-        for (l=0; l<5; l++) {
-          GT = int11_dH[i][j][k][l] -
-            (int11_dH[i][j][k][l] - int11_37[i][j][k][l])*TT;
-          pf.expint11[i][j][k][l] = exp(-GT*10./kT);
-        }
-  /* interior 2x1 loops */
-  for (i=0; i<=NBPAIRS; i++)
-    for (j=0; j<=NBPAIRS; j++)
-      for (k=0; k<5; k++)
-        for (l=0; l<5; l++) {
-          int m;
-          for (m=0; m<5; m++) {
-            GT = int21_dH[i][j][k][l][m] -
-              (int21_dH[i][j][k][l][m] - int21_37[i][j][k][l][m])*TT;
-            pf.expint21[i][j][k][l][m] = exp(-GT*10./kT);
-          }
-        }
-
-  /* interior 2x2 loops */
-  for (i=0; i<=NBPAIRS; i++)
-    for (j=0; j<=NBPAIRS; j++)
-      for (k=0; k<5; k++)
-        for (l=0; l<5; l++) {
-          int m,n;
-          for (m=0; m<5; m++)
-            for (n=0; n<5; n++) {
-              GT = int22_dH[i][j][k][l][m][n] -
-                (int22_dH[i][j][k][l][m][n]-int22_37[i][j][k][l][m][n])*TT;
-              pf.expint22[i][j][k][l][m][n] = exp(-GT*10./kT);
-            }
-        }
-
-  strncpy(pf.Tetraloops, Tetraloops, 281);
-  strncpy(pf.Triloops, Triloops, 241);
-  strncpy(pf.Hexaloops, Hexaloops, 361);
-
-  pf.id = ++pf_id;
-  return &pf;
-#endif
 }
