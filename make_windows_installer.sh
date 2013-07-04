dc5e0179c1df4c7bbb9450e85362763ef6fcd6e3
Ronny Lorenz <ronny@tbi.univie.ac.at>
2013-07-04 15:29:08 +0200

    - added a dircategory/dirsection in man/RNAlib.texinfo - made fedora mingw installation default for windows installer script

--- make_windows_installer.sh
+++ make_windows_installer.sh
@@ -10,40 +10,39 @@ export ac_cv_func_malloc_0_nonnull=yes
 
 case "$1" in
 
-fedora)       echo "Making Windows Installer using Fedora installation of mingw64"
-              echo -ne "...making 32bit version"
-              ./configure --host=i686-w64-mingw32 --without-perl --without-forester --with-cluster >> ${WIN_INSTALLER_LOG}
-              make clean  >> ${WIN_INSTALLER_LOG}
-              make >> ${WIN_INSTALLER_LOG}
-              makensis windows_installer_fedora_i686.nsi >> ${WIN_INSTALLER_LOG}
-              echo -ne "done\n"
-              echo -ne "...making 64bit version"
-              ./configure --host=x86_64-w64-mingw32 --without-perl --without-forester --with-cluster >> ${WIN_INSTALLER_LOG}
-              make clean  >> ${WIN_INSTALLER_LOG}
-              make >> ${WIN_INSTALLER_LOG}
-              makensis windows_installer_fedora_x86_64.nsi >> ${WIN_INSTALLER_LOG}
-              echo -ne "done\n"
-              ;;
-
 arch-mingw64) echo "Making Windows Installer using Arch Linux mingw32 installation of mingw-w64"
-              echo -ne "...making 32bit version"
-              ./configure --host=i486-mingw32 --without-perl --without-forester --with-cluster >> ${WIN_INSTALLER_LOG}
-              make clean >> ${WIN_INSTALLER_LOG}
-              make >> ${WIN_INSTALLER_LOG}
-              makensis windows_installer_archlinux_i486.nsi >> ${WIN_INSTALLER_LOG}
+              echo -ne "...making 32bit version..."
+              ./configure --host=i486-mingw32 --without-perl --without-forester --with-cluster &>> ${WIN_INSTALLER_LOG}
+              make clean &>> ${WIN_INSTALLER_LOG}
+              make &>> ${WIN_INSTALLER_LOG}
+              makensis win_installer_archlinux_i486.nsi &>> ${WIN_INSTALLER_LOG}
+              echo -ne " done\n"
+              ;;
+
+arch)         echo "Making Windows Installer using Arch Linux mingw32 installation of mingw"
+              echo -ne "...making 32bit version..."
+              ./configure --host=i486-mingw32 --without-perl --without-forester --with-cluster &>> ${WIN_INSTALLER_LOG}
+              make clean &>> ${WIN_INSTALLER_LOG}
+              make &>> ${WIN_INSTALLER_LOG}
+              makensis win_installer_archlinux_i486.nsi &>> ${WIN_INSTALLER_LOG}
+              echo -ne " done\n"
+              ;;
+
+*)            echo "Making Windows Installer using Fedora installation of mingw64"
+              echo -ne "...making 32bit version..."
+              mingw32-configure --without-perl --without-forester --with-cluster &>> ${WIN_INSTALLER_LOG}
+              make clean  &>> ${WIN_INSTALLER_LOG}
+              make &>> ${WIN_INSTALLER_LOG}
+              makensis win_installer_fedora_i686.nsi >> ${WIN_INSTALLER_LOG}
+              echo -ne " done\n"
+              echo -ne "...making 64bit version..."
+              mingw64-configure --without-perl --without-forester --with-cluster &>> ${WIN_INSTALLER_LOG}
+              make clean  &>> ${WIN_INSTALLER_LOG}
+              make &>> ${WIN_INSTALLER_LOG}
+              makensis win_installer_fedora_x86_64.nsi &>> ${WIN_INSTALLER_LOG}
               echo -ne "done\n"
               ;;
 
-
-
-*)            echo "Making Windows Installer using Arch Linux mingw32 installation of mingw"
-              echo -ne "...making 32bit version"
-              ./configure --host=i486-mingw32 --without-perl --without-forester --with-cluster >> ${WIN_INSTALLER_LOG}
-              make clean >> ${WIN_INSTALLER_LOG}
-              make >> ${WIN_INSTALLER_LOG}
-              makensis windows_installer_archlinux_i486.nsi >> ${WIN_INSTALLER_LOG}
-              echo -ne "done\n"
-              ;;
 esac
 
 
