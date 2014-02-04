140528f48aa0764b454cbb95fb4468ab78c9c18f
Dominik Luntzer <dominik.luntzer@gmx.at>
2014-02-04 09:52:48 +0100

    Changed some suiterunner settings
    Enable verbose output and disable forking, which greatly simplifies
    debugging of the testapplication.
    
    Signed-off-by: Ronny Lorenz <ronny@tbi.univie.ac.at>

--- tests/check_fold.c
+++ tests/check_fold.c
@@ -43,7 +43,8 @@ main(void)
   int number_failed;
   Suite *s      = fold_suite();
   SRunner *sr   = srunner_create(s);
-  srunner_run_all(sr, CK_NORMAL);
+  srunner_set_fork_status(sr, CK_NOFORK);
+  srunner_run_all(sr, CK_VERBOSE);
   number_failed = srunner_ntests_failed(sr);
   srunner_free(sr);
 
