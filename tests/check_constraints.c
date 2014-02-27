8e667002cb8055a4bd48e556858a47bc6a4e619e
Dominik Luntzer <dominik.luntzer@gmx.at>
2014-02-27 08:48:04 +0100

    Avoid duplicated calls to parse_soft_constraints_file
    For some reason the ck_assert_int_eq macro calls the given function twice.
    Use an integer with the returnvalue instead of the functioncall as
    macro argument to avoid the duplicated call.

--- tests/check_constraints.c
+++ tests/check_constraints.c
@@ -63,24 +63,28 @@ START_TEST(test_parse_soft_constraints_file)
   const size_t len = 5;
   char sequence[len];
   double values[len];
+  int ret;
 
   //1 entry
   writeTempFile(tempfile, "1 A 0.5\n");
-  ck_assert_int_eq(parse_soft_constraints_file(tempfile, 1, 0, sequence, values), 1);
+  ret = parse_soft_constraints_file(tempfile, 1, 0, sequence, values);
+  ck_assert_int_eq(ret, 1);
   ck_assert_str_eq(sequence, "A");
   ck_assert(deltaCompare(values[1], 0.5));
   unlink(tempfile);
 
   //1 entry 2 columns
   writeTempFile(tempfile, "1 0.5\n");
-  ck_assert_int_eq(parse_soft_constraints_file(tempfile, 1, 0, sequence, values), 1);
+  ret = parse_soft_constraints_file(tempfile, 1, 0, sequence, values);
+  ck_assert_int_eq(ret, 1);
   ck_assert_str_eq(sequence, "N");
   ck_assert(deltaCompare(values[1], 0.5));
   unlink(tempfile);
 
   //multiple entries
   writeTempFile(tempfile, "1 A 0.1\n2 T 0.2\n3 G 0.3\n4 C 0.4\n");
-  ck_assert_int_eq(parse_soft_constraints_file(tempfile, 4, 0, sequence, values), 1);
+  ret = parse_soft_constraints_file(tempfile, 4, 0, sequence, values);
+  ck_assert_int_eq(ret, 1);
   ck_assert_str_eq(sequence, "ATGC");
   ck_assert(deltaCompare(values[1], 0.1));
   ck_assert(deltaCompare(values[2], 0.2));
@@ -90,7 +94,8 @@ START_TEST(test_parse_soft_constraints_file)
 
   //value formats
   writeTempFile(tempfile, "1 A 1\n2 T 2.\n3 G .3\n4 C 1e-1\n");
-  ck_assert_int_eq(parse_soft_constraints_file(tempfile, 4, 0, sequence, values), 1);
+  ret = parse_soft_constraints_file(tempfile, 4, 0, sequence, values);
+  ck_assert_int_eq(ret, 1);
   ck_assert_str_eq(sequence, "ATGC");
   ck_assert(deltaCompare(values[1], 1));
   ck_assert(deltaCompare(values[2], 2));
@@ -100,7 +105,8 @@ START_TEST(test_parse_soft_constraints_file)
 
   //whitespaces
   writeTempFile(tempfile, "1 \t 0.5\n2    A\t1\n");
-  ck_assert_int_eq(parse_soft_constraints_file(tempfile, 2, 0, sequence, values), 1);
+  ret = parse_soft_constraints_file(tempfile, 2, 0, sequence, values);
+  ck_assert_int_eq(ret, 1);
   ck_assert_str_eq(sequence, "NA");
   ck_assert(deltaCompare(values[1], 0.5));
   ck_assert(deltaCompare(values[2], 1));
@@ -108,31 +114,36 @@ START_TEST(test_parse_soft_constraints_file)
 
   //missing value
   writeTempFile(tempfile, "1 A\n");
-  ck_assert_int_eq(parse_soft_constraints_file(tempfile, 1, 123, sequence, values), 1);
+  ret = parse_soft_constraints_file(tempfile, 1, 123, sequence, values);
+  ck_assert_int_eq(ret, 1);
   ck_assert_str_eq(sequence, "A");
   ck_assert(deltaCompare(values[1], 123));
   unlink(tempfile);
 
   //missing nucleotide & value
   writeTempFile(tempfile, "1\n");
-  ck_assert_int_eq(parse_soft_constraints_file(tempfile, 1, 123, sequence, values), 1);
+  ret = parse_soft_constraints_file(tempfile, 1, 123, sequence, values);
+  ck_assert_int_eq(ret, 1);
   ck_assert_str_eq(sequence, "N");
   ck_assert(deltaCompare(values[1], 123));
   unlink(tempfile);
 
   //upper limit
   writeTempFile(tempfile, "1 A 0.1\n2 T 0.2\n3 G 0.3\n4 C 0.4\n");
-  ck_assert_int_eq(parse_soft_constraints_file(tempfile, 3, 0, sequence, values), 0);
+  ret = parse_soft_constraints_file(tempfile, 3, 0, sequence, values);
+  ck_assert_int_eq(ret, 0);
   unlink(tempfile);
 
   //lower limit
   writeTempFile(tempfile, "0 A 0.1\n1 T 0.2\n2 G 0.3\n3 C 0.4\n");
-  ck_assert_int_eq(parse_soft_constraints_file(tempfile, 4, 0, sequence, values), 0);
+  ret = parse_soft_constraints_file(tempfile, 4, 0, sequence, values);
+  ck_assert_int_eq(ret, 0);
   unlink(tempfile);
 
   //unordered
   writeTempFile(tempfile, "3 G 0.3\n2 T 0.2\n4 C 0.4\n1 A 0.1\n");
-  ck_assert_int_eq(parse_soft_constraints_file(tempfile, 4, 0, sequence, values), 1);
+  ret = parse_soft_constraints_file(tempfile, 4, 0, sequence, values);
+  ck_assert_int_eq(ret, 1);
   ck_assert_str_eq(sequence, "ATGC");
   ck_assert(deltaCompare(values[1], 0.1));
   ck_assert(deltaCompare(values[2], 0.2));
@@ -142,7 +153,8 @@ START_TEST(test_parse_soft_constraints_file)
 
   //missing indices middle
   writeTempFile(tempfile, "1 A 0.1\n4 C 0.4\n");
-  ck_assert_int_eq(parse_soft_constraints_file(tempfile, 4, 123, sequence, values), 1);
+  ret = parse_soft_constraints_file(tempfile, 4, 123, sequence, values);
+  ck_assert_int_eq(ret, 1);
   ck_assert_str_eq(sequence, "ANNC");
   ck_assert(deltaCompare(values[1], 0.1));
   ck_assert(deltaCompare(values[2], 123));
@@ -152,7 +164,8 @@ START_TEST(test_parse_soft_constraints_file)
 
   //missing indices start end
   writeTempFile(tempfile, "2 T 0.2\n3 G 0.3\n");
-  ck_assert_int_eq(parse_soft_constraints_file(tempfile, 4, 123, sequence, values), 1);
+  ret = parse_soft_constraints_file(tempfile, 4, 123, sequence, values);
+  ck_assert_int_eq(ret, 1);
   ck_assert_str_eq(sequence, "NTGN");
   ck_assert(deltaCompare(values[1], 123));
   ck_assert(deltaCompare(values[2], 0.2));
@@ -161,30 +174,35 @@ START_TEST(test_parse_soft_constraints_file)
   unlink(tempfile);
 
   //invalid file
-  ck_assert_int_eq(parse_soft_constraints_file(NULL, 0, 0, sequence, values), 0);
+  ret = parse_soft_constraints_file(NULL, 0, 0, sequence, values);
+  ck_assert_int_eq(ret, 0);
 
   //missing linebreak
   writeTempFile(tempfile, "1 A 0.5");
-  ck_assert_int_eq(parse_soft_constraints_file(tempfile, 1, 0, sequence, values), 1);
+  ret = parse_soft_constraints_file(tempfile, 1, 0, sequence, values);
+  ck_assert_int_eq(ret, 1);
   ck_assert_str_eq(sequence, "A");
   ck_assert(deltaCompare(values[1], 0.5));
   unlink(tempfile);
 
   //garbage + entry
   writeTempFile(tempfile, "\nblablabla\n#evil_comment 123\n1 A 0.5\n\ngarbage\n");
-  ck_assert_int_eq(parse_soft_constraints_file(tempfile, 1, 0, sequence, values), 1);
+  ret = parse_soft_constraints_file(tempfile, 1, 0, sequence, values);
+  ck_assert_int_eq(ret, 1);
   ck_assert_str_eq(sequence, "A");
   ck_assert(deltaCompare(values[1], 0.5));
   unlink(tempfile);
 
   //garbage only
   writeTempFile(tempfile, "\nblablabla\n\ngarbage\n");
-  ck_assert_int_eq(parse_soft_constraints_file(tempfile, 1, 0, sequence, values), 0);
+  ret = parse_soft_constraints_file(tempfile, 1, 0, sequence, values);
+  ck_assert_int_eq(ret, 0);
   unlink(tempfile);
 
   //empty file
   writeTempFile(tempfile, "");
-  ck_assert_int_eq(parse_soft_constraints_file(tempfile, 1, 0, sequence, values), 0);
+  ret = parse_soft_constraints_file(tempfile, 1, 0, sequence, values);
+  ck_assert_int_eq(ret, 0);
   unlink(tempfile);
 }
 END_TEST
