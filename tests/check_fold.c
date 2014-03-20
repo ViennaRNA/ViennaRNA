690a2f58987dce779371f4ede13a26d6bc15329f
Dominik Luntzer <dominik.luntzer@gmx.at>
2014-03-20 08:23:40 +0100

    Added sanity check for stochastic backtracking
    Call vrna_pbacktrack and vrna_pbacktrack5 and check the length of the
    returned structure.

--- tests/check_fold.c
+++ tests/check_fold.c
@@ -3,6 +3,8 @@
 
 #include <check.h>
 #include <ViennaRNA/fold.h>
+#include <ViennaRNA/fold_vars.h>
+#include <ViennaRNA/part_func.h>
 
 
 START_TEST(test_fold)
@@ -14,6 +16,33 @@ START_TEST(test_fold)
   float en = fold(seq1, structure);
   ck_assert(strcmp(str1, structure) == 0);
   free(structure);
+}
+END_TEST
+
+START_TEST(test_sample_structure)
+{
+  model_detailsT md;
+  vrna_fold_compound *vc;
+  const char sequence[] = "UGCCUGGCGGCCGUAGCGCGGUGGUCCCACCUGACCCCAUGCCGAACUCAGAAGUGAAACGCCGUAGCGCCGAUGGUAGUGUGGGGUCUCCCCAUGCGAGAGUAGGGAACUGCCAGGCAU";
+  char *sample;
+
+  set_model_details(&md);
+  md.uniq_ML = 1;
+  md.do_backtrack = 0;
+
+  vc = vrna_get_fold_compound(sequence, &md, VRNA_OPTION_PF);
+
+  vrna_pf_fold(vc, NULL);
+
+  sample = vrna_pbacktrack(vc);
+  ck_assert_int_eq(strlen(sample), sizeof(sequence) - 1);
+  free(sample);
+
+  sample = vrna_pbacktrack5(vc, 16);
+  ck_assert_int_eq(strlen(sample), 16);
+  free(sample);
+
+  destroy_fold_compound(vc);
 }
 END_TEST
 
@@ -29,6 +58,7 @@ fold_suite(void)
   /* Core test case */
   TCase *tc_core = tcase_create("Core");
   tcase_add_test(tc_core, test_fold);
+  tcase_add_test(tc_core, test_sample_structure);
   suite_add_tcase(s, tc_core);
 
   suite_add_tcase(s, constraints_testcase());
