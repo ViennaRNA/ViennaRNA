b2bb721d1bef0e2d08dd64796e7e2fb759e41425
Ronny Lorenz <ronny@tbi.univie.ac.at>
2015-02-09 15:25:35 +0100

    Fixed rebase duplicate block

--- src/ViennaRNA/subopt.c
+++ src/ViennaRNA/subopt.c
@@ -1388,17 +1388,6 @@ repeat_gquad( vrna_fold_compound *vc,
   S1    = vc->sequence_encoding;
   P     = vc->params;
 
-  int *ggg, *indx, element_energy;
-  short *S, *S1;
-  paramT *P;
-
-  indx  = vc->jindx;
-  cp    = vc->cutpoint;
-  ggg   = vc->matrices->ggg;
-  S     = vc->sequence_encoding2;
-  S1    = vc->sequence_encoding;
-  P     = vc->params;
-
   /* find all gquads that fit into the energy range and the interval [i,j] */
   STATE *new_state;
   best_energy += part_energy; /* energy of current structural element */
