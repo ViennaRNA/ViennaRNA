6469fdfba46c55a35c9cb004bc6e64f5ea2de871
Ronny Lorenz <ronny@tbi.univie.ac.at>
2015-11-25 17:41:52 +0100

    Added missing .i files in interfaces/ makefiles

--- interfaces/generic.mk
+++ interfaces/generic.mk
@@ -3,13 +3,24 @@ SWIG_main_src = $(srcdir)/../RNA.i
 
 SWIG_tmaps = \
   $(srcdir)/../tmaps.i \
-  $(srcdir)/../ptr2array.i
+  $(srcdir)/../ptr2array.i \
+  $(srcdir)/../md_globals_tmaps.i
 
 SWIG_misc_src = \
-  $(srcdir)/../essentials.i \
+  $(srcdir)/../compare.i \
+  $(srcdir)/../constraints.i \
+  $(srcdir)/../eval.i \
+  $(srcdir)/../fold_compound.i \
+  $(srcdir)/../inverse.i \
+  $(srcdir)/../mfe.i \
+  $(srcdir)/../model_details.i \
+  $(srcdir)/../params.i \
+  $(srcdir)/../part_func.i \
   $(srcdir)/../plotting.i \
+  $(srcdir)/../subopt.i \
   $(srcdir)/../utils.i
 
 SWIG_module_name = RNA
 
 SWIG_wrapper = RNA_wrap.c
+
