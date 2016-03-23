1da26057e50374df1fc8d3e627f224ab1b4b8976
Ronny Lorenz <ronny@tbi.univie.ac.at>
2016-03-23 16:00:45 +0100

    Add 'src/ViennaRNA/alphabet.c'
    - This file contains the nucleotide/base pair alphabet specific
      implementations

--- src/ViennaRNA/string_utils.h
+++ src/ViennaRNA/string_utils.h
@@ -107,56 +107,6 @@ void vrna_seq_toRNA(char *sequence);
 void vrna_seq_toupper(char *sequence);
 
 /**
- *  @brief Get a numerical representation of the nucleotide sequence
- *
- */
-short *vrna_seq_encode( const char *sequence,
-                        vrna_md_t *md);
-
-/**
- *  @brief Get a numerical representation of the nucleotide sequence (simple version)
- *
- */
-short *vrna_seq_encode_simple(const char *sequence,
-                              vrna_md_t *md);
-
-/**
- *  @brief  Encode a nucleotide character to numerical value
- *
- *  This function encodes a nucleotide character to its numerical representation as required by many functions in RNAlib.
- *
- *  @see  vrna_nucleotide_decode(), vrna_seq_encode()
- *
- *  @param  c   The nucleotide character to encode
- *  @param  md  The model details that determine the kind of encoding
- *  @return     The encoded nucleotide
- */
-int vrna_nucleotide_encode( char c,
-                            vrna_md_t *md);
-
-/**
- *  @brief  Decode a numerical representation of a nucleotide back into nucleotide alphabet
- *
- *  This function decodes a numerical representation of a nucleotide character back into nucleotide alphabet
- *
- *  @see  vrna_nucleotide_encode(), vrna_seq_encode()
- *
- *  @param  enc The encoded nucleotide
- *  @param  md  The model details that determine the kind of decoding
- *  @return     The decoded nucleotide character
- */
-char vrna_nucleotide_decode(int enc,
-                            vrna_md_t *md);
-
-void vrna_aln_encode( const char *sequence,
-                      short **S_p,
-                      short **s5_p,
-                      short **s3_p,
-                      char **ss_p,
-                      unsigned short **as_p,
-                      vrna_md_t *md);
-
-/**
  *  @brief Add a separating '&' character into a string according to cut-point position
  *
  *  If the cut-point position is less or equal to zero, this function just
