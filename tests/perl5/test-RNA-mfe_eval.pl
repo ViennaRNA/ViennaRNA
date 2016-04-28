484d4b7676742b2d77e81d685602b3e3be1576ca
Mario Koestl <mario-koestl@gmx.at>
2016-04-28 16:02:47 +0200

    added test-RNA-constraints.pl tests, updated all tests for test-RNA-mfe_eval.pl and test-RNA.pl created a simple getDirectory function in RNApath.pm and changed all tests to the test::More framework

--- tests/perl5/test-RNA-mfe_eval.pl
+++ tests/perl5/test-RNA-mfe_eval.pl
@@ -3,13 +3,13 @@
 ######################### We start with some black magic to print on failure.
 # (It may become useful if the test is moved to ./t subdirectory.)
 use strict;
-use Test;
+use Test::More tests => 12;
 use Data::Dumper;
 use FileHandle;
-BEGIN { plan tests => 50; }
 
 use RNA;
 use warnings;
+
 
 my $seq1  =  		"CGCAGGGAUACCCGCG";
 my $struct1=		"(((.(((...))))))";
@@ -30,28 +30,28 @@ print "test_mfe\n";
 $fc= new RNA::fold_compound($seq1);
 ($ss,$mfe) = $fc->mfe();
 printf("%s [%6.2f] \n",$ss,$mfe);
-ok($ss,$struct1);
+is($ss,$struct1);
 undef $fc;
 ##################################
 print "test_mfe_Dimer\n";
 $fc= new RNA::fold_compound($seq1Dimer);
 ($ss,$mfe) = $fc->mfe_dimer();
 printf("%s [%6.2f] \n",$ss,$mfe);
-ok($ss,$struct1Dimer);    
+is($ss,$struct1Dimer);    
 undef $fc;
 ##################################
 print "test_mfe_window\n";
 $fc= new RNA::fold_compound($seq1);
 ($ss,$mfe) = $fc->mfe();
 printf("%s [%6.2f] \n",$ss,$mfe);
-ok($ss,$struct1);
+is($ss,$struct1);
 undef $fc;
 ##################################	
 print "test_eval_structure\n";
 $fc = new RNA::fold_compound($seq1);
 $energy= $fc->eval_structure($struct1);
 printf("%s [%6.2f] \n",$struct1,$energy);
-ok(sprintf "%6.2f",$energy, -5.60);
+is(sprintf("%6.2f",$energy), sprintf("%6.2f",-5.60));
 undef $fc;
 ##################################	
 
@@ -59,7 +59,7 @@ print "test_eval_structure_pt\n";
 $fc=new RNA::fold_compound($seq1);
 $energy= $fc->eval_structure_pt($struct1_pt) /100; #/100 for dcal
 printf("%s [%6.2f] \n",$struct1,$energy);
-ok(sprintf "%6.2f",$energy, -5.60);
+is(sprintf("%6.2f",$energy), sprintf("%6.2f",-5.60));
 undef $fc;
 ##################################
 print "test_eval_structure_verbose";
@@ -68,7 +68,7 @@ open($fh, ">", $outFile) || die "Couldn't open '".$outFile."' for reading becaus
 $energy = $fc->eval_structure_verbose($struct1,$fh);
 $energy = $fc->eval_structure_verbose($struct1,undef); # print to stdout
 printf("%s [%6.2f] \n",$struct1,$energy);
-ok(sprintf "%6.2f",$energy, -5.60);
+is(sprintf("%6.2f",$energy), sprintf("%6.2f",-5.60));
 undef $fc;
 undef $fh;
 ##################################
@@ -79,7 +79,7 @@ $fc=new RNA::fold_compound($seq1);
 $energy = $fc->eval_structure_pt_verbose($struct1_pt,$fh)/100;  # / 100 for dcal
 $energy = $fc->eval_structure_pt_verbose($struct1_pt,undef)/100;  # / 100 for dcal, print to stdout
 printf("%s [%6.2f] \n",$struct1,$energy);	
-ok(sprintf "%6.2f",$energy, -5.60);
+is(sprintf("%6.2f",$energy), sprintf("%6.2f",-5.60));
 undef $fc;
 undef $fh;
 ##################################	
@@ -100,7 +100,7 @@ print "test_eval_loop_pt\n";
 $fc= new RNA::fold_compound($seq1);
 $energy= $fc->eval_loop_pt(6,$struct1_pt) /100; #/100 for dcal
 printf("%s [%6.2f] \n",$struct1,$energy);	
-ok(sprintf "%6.2f",$energy, -3.3);
+is(sprintf("%6.2f",$energy), sprintf("%6.2f",-3.3));
 undef $fc;
 # ##################################	
 # 
@@ -108,25 +108,21 @@ print "test_eval_move_del\n";
 $fc = new RNA::fold_compound($seq1);
 $energy = $fc->eval_move($struct1,-7,-11);  # remove basepair (6,11) ,  $energy change should be 2.10
 printf("%s moveset (-7,-11) --> [%6.2f] \n",$struct1,$energy);	
-ok(sprintf "%6.2f",$energy, -2.10);
+is(sprintf("%6.2f",$energy), sprintf("%6.2f",2.10));
 # ##################################		
 # 
 print "test_eval_move_ins\n";
 $fc = new RNA::fold_compound($seq1);
 $energy = $fc->eval_move($struct11,7,11);  # add basepair (6,11) ,  $energy change should be -2.10
 printf("%s moveset (7,11) --> [%6.2f] \n",$struct11,$energy);	
-ok(sprintf "%6.2f",$energy, -2.10);	
+is(sprintf("%6.2f",$energy), sprintf("%6.2f",-2.10));	
 # ##################################	
 # 
 print "test_eval_move_pt_del\n";
 $fc = new RNA::fold_compound($seq1);
 $energy = $fc->eval_move_pt($struct1_pt,-7,-11) /100;  # remove basepair (6,11) ,  $energy change should be 2.10
 printf("%s moveset (-7,-11) --> [%6.2f] \n",$struct1,$energy);	
-ok(sprintf "%6.2f",$energy, -2.10);
+is(sprintf("%6.2f",$energy), sprintf("%6.2f",2.10));
 ##################################
-##################################
-##################################
-##################################
-##################################
-##################################
+
 
