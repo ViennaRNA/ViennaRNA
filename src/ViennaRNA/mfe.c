908f9b734518b7917199eab5fc64d499d247b2df
Ronny Lorenz <ronny@tbi.univie.ac.at>
2016-06-07 17:08:43 +0200

    Removed bug in mfe.c that scrambled backtracing with gquadruplex support for consensus structures
    See also c54fbaabf1b5050278b0c242afcdd8b9780fcdf5

--- src/ViennaRNA/mfe.c
+++ src/ViennaRNA/mfe.c
@@ -1166,11 +1166,11 @@ backtrack_comparative(vrna_fold_compound_t *vc,
         that should then be decomposed further...
       */
       mm = 0;
-      for(s=0;s<n_seq;s++){
-        tt = type[s];
+      for(ss=0;ss<n_seq;ss++){
+        tt = type[ss];
         if(tt == 0) tt = 7;
         if(dangle_model == 2)
-          mm += P->mismatchI[tt][S3[s][i]][S5[s][j]];
+          mm += P->mismatchI[tt][S3[ss][i]][S5[ss][j]];
         if(tt > 2)
           mm += P->TerminalAU;
       }
