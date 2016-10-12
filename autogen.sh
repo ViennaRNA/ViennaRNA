8d38dea17d2fd5e67207a2653f2a15caf084a241
Ronny Lorenz <ronny@tbi.univie.ac.at>
2016-10-12 17:17:43 +0200

    Squashed 'src/Kinfold/' content from commit a0b7e0d
    git-subtree-dir: src/Kinfold
    git-subtree-split: a0b7e0d50e6540da22657ca633b5b3a50e5e7e0f

---
+++ autogen.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+# 
+# Run this before configure
+#
+# This file blatantly ripped off from subversion.
+#
+# Note: this dependency on Perl is fine: only SVN developers use autogen.sh
+#       and we can state that dev people need Perl on their machine
+#
+
+set -e
+
+automake --version | perl -ne 'if (/\(GNU automake\) ([0-9].[0-9])/) {print;  if ($1 < 1.4) {exit 1;}}'
+
+if [ $? -ne 0 ]; then
+    echo "Error: you need automake 1.4 or later.  Please upgrade."
+    exit 1
+fi
+
+# Produce aclocal.m4, so autoconf gets the automake macros it needs
+echo "Creating aclocal.m4..."
+aclocal
+
+autoheader
+
+# Produce all the `Makefile.in's, verbosely, and create neat missing things
+# like `libtool', `install-sh', etc.
+automake --add-missing --verbose --gnu
+
+# If there's a config.cache file, we may need to delete it.  
+# If we have an existing configure script, save a copy for comparison.
+if [ -f config.cache ] && [ -f configure ]; then
+  cp configure configure.$$.tmp
+fi
+
+# Produce ./configure
+echo "Creating configure..."
+autoconf
+
+echo ""
+echo "You can run ./configure now."
+echo ""
+
