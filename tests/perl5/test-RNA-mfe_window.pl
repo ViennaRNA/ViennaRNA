abe3fa504a2cf820deabf90117fe65f29389542c
Ronny Lorenz <ronny@tbi.univie.ac.at>
2017-03-20 14:14:21 +0100

    Add SWIG wrappers for aliLfold_cb

--- tests/perl5/test-RNA-mfe_window.pl
+++ tests/perl5/test-RNA-mfe_window.pl
@@ -4,7 +4,7 @@
 # (It may become useful if the test is moved to ./t subdirectory.)
 
 use strict;
-use Test::More tests => 11;
+use Test::More tests => 21;
 use Data::Dumper;
 use FileHandle;
 
@@ -13,6 +13,10 @@ use warnings;
 
 
 my $seq1 = "CGCAGGGAUACCCGCG";
+my $s1="CCCCAAAACGGG";
+my $s2="CCCGAAAAGGGG";
+my $s3="CCCCAAAAGGGG";
+my @ali = ($s1,$s2,$s3);
 my $fc;
 my $mfe;
 my @data = ();
@@ -56,4 +60,26 @@ is($data[0]->{'structure'}, "(((...))).");
 is(sprintf("%6.2f", $data[0]->{'energy'}), sprintf("%6.2f", -2.60));
 is($data[1]->{'structure'}, "(((.(((...))))))");
 is(sprintf("%6.2f", $data[1]->{'energy'}), sprintf("%6.2f", -5.60));
+undef $fc;
+
+
+print "test_aliLfold_cb\n";
+@data = ();
+$mfe = RNA::aliLfold_cb(\@ali, 150, \&mfe_window_callback, \@data);
+ok(scalar(@data) == 2);
+is($data[0]->{'structure'}, "(((.....)))");
+is(sprintf("%6.2f", $data[0]->{'energy'}), sprintf("%6.2f", -1.30));
+is($data[1]->{'structure'}, "(((......)))");
+is(sprintf("%6.2f", $data[1]->{'energy'}), sprintf("%6.2f", -2.70));
+
+
+print "test_mfe_window_cb (comparative)\n";
+$fc= new RNA::fold_compound(\@ali, undef, RNA::OPTION_MFE | RNA::OPTION_WINDOW);
+@data = ();
+$mfe = $fc->mfe_window_cb(\&mfe_window_callback, \@data);
+ok(scalar(@data) == 2);
+is($data[0]->{'structure'}, "(((.....)))");
+is(sprintf("%6.2f", $data[0]->{'energy'}), sprintf("%6.2f", -1.30));
+is($data[1]->{'structure'}, "(((......)))");
+is(sprintf("%6.2f", $data[1]->{'energy'}), sprintf("%6.2f", -2.70));
 undef $fc;
