917eff751ea5693428a40410969354c6894467a2
Ronny Lorenz <ronny@tbi.univie.ac.at>
2017-04-07 17:28:12 +0200

    Enable RNAfold unit tests to run in parallel

--- tests/RNAfold/partfunc.sh
+++ tests/RNAfold/partfunc.sh
@@ -17,12 +17,12 @@ function testline {
 
 # Test partition function folding (centroid, MEA, base pair- and stack probabilities)
 testline "Partition function (centroid, MEA)"
-RNAfold --noPS -p --MEA < ${DATADIR}/rnafold.small.seq > tmp.fold
-diff=$(${DIFF} -I frequency ${RNAFOLD_RESULTSDIR}/rnafold.small.pf.gold tmp.fold)
+RNAfold --noPS -p --MEA < ${DATADIR}/rnafold.small.seq > rnafold_pf.fold
+diff=$(${DIFF} -I frequency ${RNAFOLD_RESULTSDIR}/rnafold.small.pf.gold rnafold_pf.fold)
 if [ "x${diff}" != "x" ] ; then failed; echo -e "$diff"; else passed; fi
 
 testline "Partition function (base pair probabilities)"
-RNAfold --noPS -p2 --auto-id --id-prefix="rnafold_pf_test" < ${DATADIR}/rnafold.small.seq > tmp.fold
+RNAfold --noPS -p2 --auto-id --id-prefix="rnafold_pf_test" < ${DATADIR}/rnafold.small.seq > rnafold_pf.fold
 for file in rnafold_pf_test_00*dp.ps
 do
   diff=$(${DIFF} -I CreationDate -I Creator ${RNAFOLD_RESULTSDIR}/${file} ${file})
@@ -39,7 +39,7 @@ done
 if [ "x${diff}" != "x" ] ; then failed; echo -e "$diff"; else passed; fi
 
 # clean up
-rm tmp.fold dot.ps
+rm rnafold_pf.fold dot.ps
 rm rnafold_pf_test_00*dp.ps rnafold_pf_test_00*dp2.ps
 
 exit ${RETURN}
