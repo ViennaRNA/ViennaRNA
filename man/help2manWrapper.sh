ff042980138b447296341dea00e15044e6f63acd
Ronny Lorenz <ronny@tbi.univie.ac.at>
2018-05-09 12:05:28 +0200

    Disable build of manpages and commandline parsers if required programs are missing
    - This simply hides the rules to (re-)generate the manpages through
      help2man and the commandline parsers through gengetopt if these
      programs are missing.

--- man/help2manWrapper.sh
+++ man/help2manWrapper.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 #
 
-PROGNAME=`basename $1 .ggo`
-PROGPATH=`dirname $1`
+HELP2MAN=$1
+PROGNAME=`basename $2 .ggo`
+PROGPATH=`dirname $2`
 
-help2man -N --help-option=--detailed-help \
-  --opt-include=./include/ref_package.inc \
-                --opt-include=./include/${PROGNAME}.inc \
-                --opt-include=./include/ref_energy_par.inc \
-                "./cmdlopt.sh ${PROGPATH}/${PROGNAME}"
+${HELP2MAN} \
+    -N \
+    --help-option=--detailed-help \
+    --opt-include=./include/ref_package.inc \
+    --opt-include=./include/${PROGNAME}.inc \
+    --opt-include=./include/ref_energy_par.inc \
+    "./cmdlopt.sh ${PROGPATH}/${PROGNAME}"
