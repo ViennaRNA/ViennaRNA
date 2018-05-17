0f305c706a70056106e3526f963b456e808da7cc
Ronny Lorenz <ronny@tbi.univie.ac.at>
2018-05-17 17:18:02 +0200

    CLEANUP: Uncrustify centroid.[c,h]

--- src/ViennaRNA/centroid.c
+++ src/ViennaRNA/centroid.c
@@ -1,9 +1,9 @@
 /*
-                  centroid structure prediction
-
-                  Ivo L Hofacker + Ronny Lorenz
-                  Vienna RNA package
-*/
+ *                centroid structure prediction
+ *
+ *                Ivo L Hofacker + Ronny Lorenz
+ *                Vienna RNA package
+ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -20,41 +20,42 @@
 #include "ViennaRNA/centroid.h"
 
 /*
-#################################
-# GLOBAL VARIABLES              #
-#################################
-*/
+ #################################
+ # GLOBAL VARIABLES              #
+ #################################
+ */
 
 /*
-#################################
-# PRIVATE VARIABLES             #
-#################################
-*/
+ #################################
+ # PRIVATE VARIABLES             #
+ #################################
+ */
 
 /*
-#################################
-# PRIVATE FUNCTION DECLARATIONS #
-#################################
-*/
+ #################################
+ # PRIVATE FUNCTION DECLARATIONS #
+ #################################
+ */
 
 /*
-#################################
-# BEGIN OF FUNCTION DEFINITIONS #
-#################################
-*/
+ #################################
+ # BEGIN OF FUNCTION DEFINITIONS #
+ #################################
+ */
 
+/* compute the centroid structure of the ensemble, i.e. the strutcure
+ * with the minimal average distance to all other structures
+ * <d(S)> = \sum_{(i,j) \in S} (1-p_{ij}) + \sum_{(i,j) \notin S} p_{ij}
+ * Thus, the centroid is simply the structure containing all pairs with
+ * p_ij>0.5
+ */
 PUBLIC char *
-vrna_centroid_from_plist( int length,
-                          double *dist,
-                          vrna_ep_t *pl){
-
-  /* compute the centroid structure of the ensemble, i.e. the strutcure
-     with the minimal average distance to all other structures
-     <d(S)> = \sum_{(i,j) \in S} (1-p_{ij}) + \sum_{(i,j) \notin S} p_{ij}
-     Thus, the centroid is simply the structure containing all pairs with
-     p_ij>0.5 */
-  int i;
-  char *centroid;
+vrna_centroid_from_plist(int        length,
+                         double     *dist,
+                         vrna_ep_t  *pl)
+{
+  int   i;
+  char  *centroid;
 
   if (pl == NULL) {
     vrna_message_warning("vrna_centroid_from_plist: "
@@ -62,35 +63,39 @@ vrna_centroid_from_plist( int length,
     return NULL;
   }
 
-  *dist = 0.;
-  centroid = (char *) vrna_alloc((length+1)*sizeof(char));
-  for (i=0; i<length; i++) centroid[i]='.';
-  for (i=0; pl[i].i>0; i++){
-    if ((pl[i].p)>0.5) {
-      centroid[pl[i].i-1] = '(';
-      centroid[pl[i].j-1] = ')';
-      *dist += (1-pl[i].p);
-    } else
+  *dist     = 0.;
+  centroid  = (char *)vrna_alloc((length + 1) * sizeof(char));
+  for (i = 0; i < length; i++)
+    centroid[i] = '.';
+  for (i = 0; pl[i].i > 0; i++) {
+    if ((pl[i].p) > 0.5) {
+      centroid[pl[i].i - 1] = '(';
+      centroid[pl[i].j - 1] = ')';
+      *dist                 += (1 - pl[i].p);
+    } else {
       *dist += pl[i].p;
+    }
   }
   centroid[length] = '\0';
   return centroid;
 }
 
-PUBLIC char *
-vrna_centroid_from_probs( int length,
-                          double *dist,
-                          FLT_OR_DBL *probs){
 
-  /* compute the centroid structure of the ensemble, i.e. the strutcure
-     with the minimal average distance to all other structures
-     <d(S)> = \sum_{(i,j) \in S} (1-p_{ij}) + \sum_{(i,j) \notin S} p_{ij}
-     Thus, the centroid is simply the structure containing all pairs with
-     p_ij>0.5 */
-  int i,j;
-  FLT_OR_DBL p;
-  char  *centroid;
-  int   *index = vrna_idx_row_wise(length);
+/* compute the centroid structure of the ensemble, i.e. the strutcure
+ * with the minimal average distance to all other structures
+ * <d(S)> = \sum_{(i,j) \in S} (1-p_{ij}) + \sum_{(i,j) \notin S} p_{ij}
+ * Thus, the centroid is simply the structure containing all pairs with
+ * p_ij>0.5
+ */
+PUBLIC char *
+vrna_centroid_from_probs(int        length,
+                         double     *dist,
+                         FLT_OR_DBL *probs)
+{
+  int         i, j;
+  FLT_OR_DBL  p;
+  char        *centroid;
+  int         *index = vrna_idx_row_wise(length);
 
   if (probs == NULL) {
     vrna_message_warning("vrna_centroid_from_probs: "
@@ -98,90 +103,98 @@ vrna_centroid_from_probs( int length,
     return NULL;
   }
 
-  *dist = 0.;
-  centroid = (char *) vrna_alloc((length+1)*sizeof(char));
-  for (i=0; i<length; i++) centroid[i]='.';
-  for (i=1; i<=length; i++)
-    for (j=i+TURN+1; j<=length; j++) {
-      if ((p=probs[index[i]-j])>0.5) {
-        centroid[i-1] = '(';
-        centroid[j-1] = ')';
-        *dist += (1-p);
-      } else
+  *dist     = 0.;
+  centroid  = (char *)vrna_alloc((length + 1) * sizeof(char));
+  for (i = 0; i < length; i++)
+    centroid[i] = '.';
+  for (i = 1; i <= length; i++)
+    for (j = i + TURN + 1; j <= length; j++) {
+      if ((p = probs[index[i] - j]) > 0.5) {
+        centroid[i - 1] = '(';
+        centroid[j - 1] = ')';
+        *dist           += (1 - p);
+      } else {
         *dist += p;
+      }
     }
   free(index);
   centroid[length] = '\0';
   return centroid;
 }
 
-PUBLIC char *
-vrna_centroid(vrna_fold_compound_t *vc,
-             double *dist){
 
-  /* compute the centroid structure of the ensemble, i.e. the strutcure
-     with the minimal average distance to all other structures
-     <d(S)> = \sum_{(i,j) \in S} (1-p_{ij}) + \sum_{(i,j) \notin S} p_{ij}
-     Thus, the centroid is simply the structure containing all pairs with
-     p_ij>0.5 */
-  int i,j, k, length;
-  FLT_OR_DBL p;
-  char  *centroid;
-  short *S;
+/* compute the centroid structure of the ensemble, i.e. the strutcure
+ * with the minimal average distance to all other structures
+ * <d(S)> = \sum_{(i,j) \in S} (1-p_{ij}) + \sum_{(i,j) \notin S} p_{ij}
+ * Thus, the centroid is simply the structure containing all pairs with
+ * p_ij>0.5
+ */
+PUBLIC char *
+vrna_centroid(vrna_fold_compound_t  *vc,
+              double                *dist)
+{
+  int               i, j, k, length;
+  FLT_OR_DBL        p;
+  char              *centroid;
+  short             *S;
   vrna_mx_pf_t      *matrices;
   FLT_OR_DBL        *probs;
   int               *my_iindx;
   vrna_exp_param_t  *pf_params;
 
 
-  if(!vc){
+  if (!vc) {
     vrna_message_warning("vrna_centroid: "
                          "run vrna_pf_fold first!");
     return NULL;
-  } else if( !vc->exp_matrices->probs) {
+  } else if (!vc->exp_matrices->probs) {
     vrna_message_warning("vrna_centroid: "
                          "probs == NULL!");
     return NULL;
   }
 
-  length      = vc->length;
-  pf_params   = vc->exp_params;
-  S           = vc->sequence_encoding2;
-  my_iindx    = vc->iindx;
+  length    = vc->length;
+  pf_params = vc->exp_params;
+  S         = vc->sequence_encoding2;
+  my_iindx  = vc->iindx;
 
-  matrices    = vc->exp_matrices;
-  probs       = matrices->probs;
+  matrices  = vc->exp_matrices;
+  probs     = matrices->probs;
 
-  *dist = 0.;
-  centroid = (char *) vrna_alloc((length+1)*sizeof(char));
-  for (i=0; i<length; i++) centroid[i]='.';
-  for (i=1; i<=length; i++)
-    for (j=i+TURN+1; j<=length; j++) {
-      if ((p=probs[my_iindx[i]-j])>0.5) {
-        if(pf_params->model_details.gquad){
+  *dist     = 0.;
+  centroid  = (char *)vrna_alloc((length + 1) * sizeof(char));
+  for (i = 0; i < length; i++)
+    centroid[i] = '.';
+  for (i = 1; i <= length; i++)
+    for (j = i + TURN + 1; j <= length; j++) {
+      if ((p = probs[my_iindx[i] - j]) > 0.5) {
+        if (pf_params->model_details.gquad) {
           /* check for presence of gquadruplex */
-          if((S[i] == 3) && (S[j] == 3)){
+          if ((S[i] == 3) && (S[j] == 3)) {
             int L, l[3];
             get_gquad_pattern_pf(S, i, j, pf_params, &L, l);
-            for(k=0;k<L;k++){
-              centroid[i+k-1]\
-              = centroid[i+k+L+l[0]-1]\
-              = centroid[i+k+2*L+l[0]+l[1]-1]\
-              = centroid[i+k+3*L+l[0]+l[1]+l[2]-1]\
-              = '+';
+            for (k = 0; k < L; k++) {
+              centroid[i + k - 1] \
+                            = centroid[i + k + L + l[0] - 1] \
+                            = centroid[i + k + 2 * L + l[0] + l[1] - 1] \
+                            = centroid[i + k + 3 * L + l[0] + l[1] + l[2] - 1] \
+                            = '+';
             }
             /* skip everything within the gquad */
-            i = j; j = j+TURN+1;
-            *dist += (1-p); /* right? */
+            i     = j;
+            j     = j + TURN + 1;
+            *dist += (1 - p); /* right? */
             break;
           }
         }
+
         /* regular base pair */
-        centroid[i-1] = '(';
-        centroid[j-1] = ')';
-        *dist += (1-p);
-      } else
+        centroid[i - 1] = '(';
+        centroid[j - 1] = ')';
+        *dist           += (1 - p);
+      } else {
         *dist += p;
+      }
     }
 
   centroid[length] = '\0';
@@ -196,19 +209,19 @@ vrna_centroid(vrna_fold_compound_t *vc,
 
 /* this function is a threadsafe replacement for centroid() */
 PUBLIC char *
-get_centroid_struct_pl( int length,
-                        double *dist,
-                        vrna_ep_t *pl){
-
+get_centroid_struct_pl(int        length,
+                       double     *dist,
+                       vrna_ep_t  *pl)
+{
   return vrna_centroid_from_plist(length, dist, pl);
 }
 
+
 /* this function is a threadsafe replacement for centroid() */
 PUBLIC char *
-get_centroid_struct_pr( int length,
-                        double *dist,
-                        FLT_OR_DBL *probs){
-
+get_centroid_struct_pr(int        length,
+                       double     *dist,
+                       FLT_OR_DBL *probs)
+{
   return vrna_centroid_from_probs(length, dist, probs);
 }
-
--- src/ViennaRNA/centroid.h
+++ src/ViennaRNA/centroid.h
@@ -24,25 +24,26 @@
 
 /**
  *  @brief Get the centroid structure of the ensemble
- * 
+ *
  *  The centroid is the structure with the minimal average distance to all other structures
  *  \n @f$ <d(S)> = \sum_{(i,j) \in S} (1-p_{ij}) + \sum_{(i,j) \notin S} p_{ij} @f$ \n
  *  Thus, the centroid is simply the structure containing all pairs with @f$p_ij>0.5@f$
  *  The distance of the centroid to the ensemble is written to the memory adressed by @a dist.
- * 
+ *
  *  @ingroup              centroid_fold
  *  @param[in]    vc      The fold compound data structure
  *  @param[out]   dist    A pointer to the distance variable where the centroid distance will be written to
  *  @return               The centroid structure of the ensemble in dot-bracket notation (@NULL on error)
  */
-char *vrna_centroid(vrna_fold_compound_t *vc,
-                    double *dist);
+char *vrna_centroid(vrna_fold_compound_t  *vc,
+                    double                *dist);
+
 
 /**
  *  @brief Get the centroid structure of the ensemble
- * 
+ *
  *  This function is a threadsafe replacement for @ref centroid() with a #vrna_ep_t input
- * 
+ *
  *  The centroid is the structure with the minimal average distance to all other structures
  *  \n @f$ <d(S)> = \sum_{(i,j) \in S} (1-p_{ij}) + \sum_{(i,j) \notin S} p_{ij} @f$ \n
  *  Thus, the centroid is simply the structure containing all pairs with @f$p_ij>0.5@f$
@@ -54,29 +55,31 @@ char *vrna_centroid(vrna_fold_compound_t *vc,
  *  @param[in]  pl      A pair list containing base pair probability information about the ensemble
  *  @return             The centroid structure of the ensemble in dot-bracket notation (@NULL on error)
  */
-char  *vrna_centroid_from_plist(int length,
-                                double *dist,
-                                vrna_ep_t *pl);
+char *vrna_centroid_from_plist(int        length,
+                               double     *dist,
+                               vrna_ep_t  *pl);
+
 
 /**
  *  @brief Get the centroid structure of the ensemble
- * 
+ *
  *  This function is a threadsafe replacement for @ref centroid() with a probability array input
- * 
+ *
  *  The centroid is the structure with the minimal average distance to all other structures
  *  \n @f$ <d(S)> = \sum_{(i,j) \in S} (1-p_{ij}) + \sum_{(i,j) \notin S} p_{ij} @f$ \n
  *  Thus, the centroid is simply the structure containing all pairs with @f$p_ij>0.5@f$
  *  The distance of the centroid to the ensemble is written to the memory adressed by @a dist.
- * 
+ *
  *  @ingroup              centroid_fold
  *  @param[in]    length  The length of the sequence
  *  @param[out]   dist    A pointer to the distance variable where the centroid distance will be written to
  *  @param[in]    probs   An upper triangular matrix containing base pair probabilities (access via iindx @ref vrna_idx_row_wise() )
  *  @return               The centroid structure of the ensemble in dot-bracket notation (@NULL on error)
  */
-char  *vrna_centroid_from_probs(int length,
-                                double *dist,
-                                FLT_OR_DBL *probs);
+char *vrna_centroid_from_probs(int        length,
+                               double     *dist,
+                               FLT_OR_DBL *probs);
+
 
 #ifndef VRNA_DISABLE_BACKWARD_COMPATIBILITY
 
@@ -85,20 +88,20 @@ char  *vrna_centroid_from_probs(int length,
  *
  *  @deprecated This function was renamed to vrna_centroid_from_plist()
  */
-DEPRECATED(char  *get_centroid_struct_pl(int length,
-                              double *dist,
-                              vrna_ep_t *pl),
-          "Use vrna_centroid_from_plist() instead");
+DEPRECATED(char *get_centroid_struct_pl(int       length,
+                                        double    *dist,
+                                        vrna_ep_t *pl),
+           "Use vrna_centroid_from_plist() instead");
 
 /**
  *  @brief Get the centroid structure of the ensemble
  *
  *  @deprecated This function was renamed to vrna_centroid_from_probs()
  */
-DEPRECATED(char  *get_centroid_struct_pr(int length,
-                              double *dist,
-                              FLT_OR_DBL *pr),
-          "Use vrna_centroid_from_probs() instead");
+DEPRECATED(char *get_centroid_struct_pr(int         length,
+                                        double      *dist,
+                                        FLT_OR_DBL  *pr),
+           "Use vrna_centroid_from_probs() instead");
 
 #endif
 
