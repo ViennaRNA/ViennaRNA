27a0dac08d9b69adf1bb23f60e9669c202ec2e53
Ronny Lorenz <ronny@tbi.univie.ac.at>
2018-05-18 12:39:25 +0200

    REFMAN:	Update documentation in LPfold.h

--- src/ViennaRNA/LPfold.h
+++ src/ViennaRNA/LPfold.h
@@ -4,8 +4,11 @@
 /**
  *  @file     LPfold.h
  *  @ingroup  local_fold
- *  @brief    Partition function implementation for the Lfold algorithm
+ *  @brief    Partition function and equilibrium probability implementation for the sliding window algorithm
  *
+ *  This file contains the implementation for sliding window partition function and equilibrium
+ *  probabilities. It also provides the unpaired probability implementation from Bernhart et al.
+ *  2011 @cite bernhart:2011
  */
 
 
@@ -20,14 +23,32 @@
  * this function.
  * @endparblock
  *
+ * #### Types: ####
+ *  * #VRNA_PROBS_WINDOW_BPP  - @copybrief #VRNA_PROBS_WINDOW_BPP
+ *  * #VRNA_PROBS_WINDOW_UP   - @copybrief #VRNA_PROBS_WINDOW_UP
+ *  * #VRNA_PROBS_WINDOW_PF   - @copybrief #VRNA_PROBS_WINDOW_PF
+ *
+ * The above types usually come exclusively. However, for unpaired
+ * probabilities, the #VRNA_PROBS_WINDOW_UP flag is OR-ed together
+ * with one of the loop type contexts
+ *
+ *  * #VRNA_EXT_LOOP  - @copybrief #VRNA_EXT_LOOP
+ *  * #VRNA_HP_LOOP   - @copybrief #VRNA_HP_LOOP
+ *  * #VRNA_INT_LOOP  - @copybrief #VRNA_INT_LOOP
+ *  * #VRNA_MB_LOOP   - @copybrief #VRNA_MB_LOOP
+ *  * #VRNA_ANY_LOOP  - @copybrief #VRNA_ANY_LOOP
+ *
+ * to indicate the particular type of data available through the @p pr
+ * pointer.
+ *
+ * @ingroup  local_pf_fold
  * @see vrna_probs_window(), vrna_pfl_fold_up_cb()
- *      #VRNA_PROBS_WINDOW_BPP, #VRNA_PROBS_WINDOW_UP, #VRNA_PROBS_WINDOW_STACKP,
- *      #VRNA_EXT_LOOP, #VRNA_HP_LOOP, #VRNA_INT_LOOP, #VRNA_MB_LOOP, #VRNA_ANY_LOOP
  *
  * @param pr      An array of probabilities
  * @param pr_size The length of the probability array
  * @param i       The i-position (5') of the probabilities
  * @param max     The (theoretical) maximum length of the probability array
+ * @param type    The type of data that is provided
  * @param data    Auxiliary data
  */
 
@@ -54,42 +75,112 @@ typedef void (vrna_probs_window_callback)(FLT_OR_DBL    *pr,
 #endif
 
 
+/**
+ *  @brief  Exterior loop
+ */
 #define VRNA_EXT_LOOP   1U
+
+/**
+ *  @brief  Hairpin loop
+ */
 #define VRNA_HP_LOOP    2U
+
+/**
+ *  @brief  Internal loop
+ */
 #define VRNA_INT_LOOP   4U
+
+/**
+ *  @brief  Multibranch loop
+ */
 #define VRNA_MB_LOOP    8U
+
+/**
+ *  @brief  Any loop
+ */
 #define VRNA_ANY_LOOP   (VRNA_EXT_LOOP | VRNA_HP_LOOP | VRNA_INT_LOOP | VRNA_MB_LOOP)
 
 
 /**
- *  @brief  Option flag to activate callback triggers for pairing probabilities
+ *  @brief  Trigger base pairing probabilities
+ *
+ *  Passing this flag to vrna_probs_window() activates callback execution for base
+ *  pairing probabilities. In turn, the corresponding callback receives this flag
+ *  through the @p type argument whenever base pairing probabilities are provided.
+ *
+ *  Detailed information for the algorithm to compute unpaired probabilities
+ *  can be taken from @cite bernhart:2005.
+ *
  *  @see  vrna_probs_window()
  *  @ingroup  local_pf_fold
  */
 #define VRNA_PROBS_WINDOW_BPP  4096U
 
 /**
- *  @brief  Option flag to activate callback triggers for unpaired probabilities
+ *  @brief  Trigger unpaired probabilities
+ *
+ *  Passing this flag to vrna_probs_window() activates callback execution for
+ *  unpaired probabilities. In turn, the corresponding callback receives this flag
+ *  through the @p type argument whenever unpaired probabilities are provided.
+ *
+ *  Detailed information for the algorithm to compute unpaired probabilities
+ *  can be taken from @cite bernhart:2011.
+ *
  *  @see  vrna_probs_window()
  *  @ingroup  local_pf_fold
  */
 #define VRNA_PROBS_WINDOW_UP   8192U
 
 /**
- *  @brief  Option flag to activate callback triggers for stacking probabilities
+ *  @brief Trigger base pair stack probabilities
+ *
+ *  Passing this flag to vrna_probs_window() activates callback execution for
+ *  stacking probabilities. In turn, the corresponding callback receives this flag
+ *  through the @p type argument whenever stack probabilities are provided.
+ *
+ *  @bug  Currently, this flag is a placeholder doing nothing as the corresponding
+ *        implementation for stack probability computation is missing.
+ *
  *  @see  vrna_probs_window()
  *  @ingroup  local_pf_fold
  */
 #define VRNA_PROBS_WINDOW_STACKP   16384U
 
 /**
- *  @brief  Option flag to activate callback triggers for unpaired probabilities (split up into different loop types)
- *  @see  vrna_probs_window(), #VRNA_EXT_LOOP, #VRNA_HP_LOOP, #VRNA_INT_LOOP, #VRNA_MB_LOOP, #VRNA_ANY_LOOP
+ *  @brief  Trigger detailed unpaired probabilities split up into different loop type contexts
+ *
+ *  Passing this flag to vrna_probs_window() activates callback execution for
+ *  unpaired probabilities. In contrast to #VRNA_PROBS_WINDOW_UP this flag requests
+ *  unpaired probabilities to be split up into different loop type contexts. In turn,
+ *  the corresponding callback receives the #VRNA_PROBS_WINDOW_UP flag OR-ed together
+ *  with the corresponding loop type, i.e.:
+ *
+ *  * #VRNA_EXT_LOOP  - @copybrief #VRNA_EXT_LOOP
+ *  * #VRNA_HP_LOOP   - @copybrief #VRNA_HP_LOOP
+ *  * #VRNA_INT_LOOP  - @copybrief #VRNA_INT_LOOP
+ *  * #VRNA_MB_LOOP   - @copybrief #VRNA_MB_LOOP
+ *  * #VRNA_ANY_LOOP  - @copybrief #VRNA_ANY_LOOP
+ *
+ *  @see  vrna_probs_window(), #VRNA_PROBS_WINDOW_UP
  *  @ingroup  local_pf_fold
  */
 #define VRNA_PROBS_WINDOW_UP_SPLIT   32768U
 
 
+/**
+ *  @brief  Trigger partition function
+ *
+ *  Passing this flag to vrna_probs_window() activates callback execution for
+ *  partition function. In turn, the corresponding callback receives this flag
+ *  through it's @p type argument whenever partition function data is provided.
+ *
+ *  @note Instead of actually providing the partition function @f$Z@f$, the
+ *        callback is always provided with the corresponding enemble free energy
+ *        @f$\Delta G = - RT \ln Z@f$.
+ *
+ *  @see  vrna_probs_window()
+ *  @ingroup  local_pf_fold
+ */
 #define VRNA_PROBS_WINDOW_PF        65536U
 
 /**
@@ -218,8 +309,21 @@ vrna_pfl_fold_up_cb(const char                  *sequence,
 /**
  *  @brief  Compute various equilibrium probabilities under a sliding window approach
  *
- *  @see  vrna_pfl_fold_cb(), vrna_pfl_fold_up_cb(), #VRNA_PROBS_WINDOW_BPP, #VRNA_PROBS_WINDOW_UP,
- *        #VRNA_PROBS_WINDOW_UP_SPLIT
+ *  This function applies a sliding window scan for the sequence provided with the
+ *  argument @p fc and reports back equilibrium probabilities through the callback
+ *  function @p cb. The data reported to the callback depends on the @p options flag.
+ *
+ *  @note   The parameter @p ulength only affects computation and resulting data if unpaired
+ *          probability computations are requested through the @p options flag.
+ *
+ *  #### Options: ####
+ *  * #VRNA_PROBS_WINDOW_BPP      - @copybrief #VRNA_PROBS_WINDOW_BPP
+ *  * #VRNA_PROBS_WINDOW_UP       - @copybrief #VRNA_PROBS_WINDOW_UP
+ *  * #VRNA_PROBS_WINDOW_UP_SPLIT - @copybrief #VRNA_PROBS_WINDOW_UP_SPLIT
+ *
+ *  Options may be OR-ed together
+ *
+ *  @see  vrna_pfl_fold_cb(), vrna_pfl_fold_up_cb()
  *
  *  @ingroup local_pf_fold
  *  @param  fc            The fold compound with sequence data, model settings and precomputed energy parameters
