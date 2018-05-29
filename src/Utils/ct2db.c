1e94c1ee78fa514e115b827879496d01e2d56b16
Ronny Lorenz <ronny@tbi.univie.ac.at>
2018-05-29 18:19:39 +0200

    Revert "Allow for lowercase letters in ct2db input and add some fall-through comments to make gcc happy"
    This reverts commit db907216261ebfb026d958d9ee8f79d336dcda67.

--- src/Utils/ct2db.c
+++ src/Utils/ct2db.c
@@ -1,3 +1,5 @@
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -14,61 +16,54 @@
 #include "ViennaRNA/io/utils.h"
 #include "ct2db_cmdl.h"
 
-int
-main(int  argc,
-     char *argv[])
-{
+int main(int argc, char *argv[]){
   struct  ct2db_args_info args_info;
-  char                    *line       = NULL;
-  int                     pkfree      = 0;
-  int                     verbose     = 0;
-  char                    *seq        = NULL;
-  char                    *structure  = NULL;
-  plist                   *pairs      = NULL;
-  int                     num_pairs   = 0;
-  unsigned int            seq_pos     = 0;
-  unsigned int            length      = 0;
+
+  char          *line       = NULL;
+
+  int           pkfree      = 0;
+  int           verbose     = 0;
+
+  char          *seq        = NULL;
+  char          *structure  = NULL;
+  plist         *pairs      = NULL;
+  int           num_pairs   = 0;
+  unsigned int  seq_pos     = 0;
+  unsigned int  length      = 0;
 
   /*
-   #############################################
-   # check the command line prameters
-   #############################################
-   */
-  if (ct2db_cmdline_parser(argc, argv, &args_info) != 0)
-    exit(1);
-
+  #############################################
+  # check the command line prameters
+  #############################################
+  */
+  if(ct2db_cmdline_parser (argc, argv, &args_info) != 0) exit(1);
   /* remove Pseudknots? */
-  if (args_info.removePK_given)
+  if(args_info.removePK_given)
     pkfree = 1;
-
   /* be verbose ? */
-  if (args_info.verbose_given)
+  if(args_info.verbose_given)
     verbose = 1;
 
   /* free allocated memory of command line data structure */
-  ct2db_cmdline_parser_free(&args_info);
+  ct2db_cmdline_parser_free (&args_info);
 
-  do {
-    if ((line = vrna_read_line(stdin)) == NULL)
-      break;
-
+  do{
+    if ((line = vrna_read_line(stdin))==NULL) break;
     /* skip comment lines */
-    while ((*line == '*') || (*line == '\0') || (*line == '>') || (*line == '#')) {
+    while ((*line=='*')||(*line=='\0')||(*line=='>')||(*line=='#')) {
       free(line);
-      if ((line = vrna_read_line(stdin)) == NULL)
-        break;
+      if ((line = vrna_read_line(stdin))==NULL) break;
     }
 
-    if ((line == NULL) || (strcmp(line, "@") == 0))
-      break;
+    if ((line ==NULL) || (strcmp(line, "@") == 0)) break;
 
-    if (length != 0) {
-      if (!seq) {
+    if(length != 0){
+      if(!seq){
         /*
-         * printf("sequence length: %u\n", length);
-         */
+        printf("sequence length: %u\n", length);
+        */
         seq   = (char *)vrna_alloc(sizeof(char) * (length + 1));
-        pairs = (plist *)vrna_alloc(sizeof(plist) * (2 * length));
+        pairs = (plist *)vrna_alloc(sizeof(plist) * (2*length));
       }
     }
 
@@ -81,106 +76,87 @@ main(int  argc,
 
     ptr = strtok(line, " \t");
 
-    while (ptr != NULL) {
-      /*
-       *    printf("%d. %s\n", row, ptr);
-       */
+    while(ptr != NULL){
+/*
+      printf("%d. %s\n", row, ptr);
+*/
 
-      char *end;
-      curr_int = strtol(ptr, &end, 10);
-      if (*end)
-        curr_int = -1;        /* not read an int */
+      char* end;
+      curr_int          = strtol(ptr, &end, 10);
+      if(*end) curr_int = -1; /* not read an int */
 
-      switch (row) {
-        case 0:
-          if (curr_int != -1)
-            i = (int)curr_int;
-
-          break;
-        case 4:
-          if (curr_int != -1) {
-            if ((i != -1) && (curr_int > i)) {
-              j                       = curr_int;
-              pairs[num_pairs].i      = i;
-              pairs[num_pairs].j      = j;
-              pairs[num_pairs].p      = 1.;
-              pairs[num_pairs++].type = 0;
-            }
-          }
-
-          break;
-        case 1:
-          if (sscanf(ptr, "%c", &c)) {
-            switch (c) {
-              case 'E':
-                if (!strcmp("ENERGY", ptr))
-                  if (last_int != -1)
-                    length = (unsigned int)last_int;
-
-                break;
-              case 'N':         /* fall through */
-              case 'A':         /* fall through */
-              case 'U':         /* fall through */
-              case 'T':         /* fall through */
-              case 'C':         /* fall through */
-              case 'G':         /* fall through */
-              case 'n':         /* fall through */
-              case 'a':         /* fall through */
-              case 'u':         /* fall through */
-              case 'c':         /* fall through */
-              case 'g':         /* fall through */
-              case 't':
-                if (seq_pos < length)
-                  seq[seq_pos++] = toupper(c);
-
-                break;
-              default:
-                break;
-            }
-          }
-
-          break;
-        default:
-          break;
+      switch(row){
+        
+        case 0:   if( curr_int != -1)
+                    i = (int) curr_int;
+                  break;
+        case 4:   if( curr_int != -1 )
+                    if((i != -1) && (curr_int > i)){
+                      j = curr_int;
+                      pairs[num_pairs].i      = i;
+                      pairs[num_pairs].j      = j;
+                      pairs[num_pairs].p      = 1.;
+                      pairs[num_pairs++].type = 0;
+                    }
+                  break;
+        case 1:   if(sscanf(ptr, "%c", &c)){
+                    switch(c){
+                      case 'E': if(!strcmp("ENERGY", ptr)){
+                                  if(last_int != -1)
+                                  length = (unsigned int)last_int;
+                                }
+                                break;
+                      case 'A':
+                      case 'U':
+                      case 'T':
+                      case 'C':
+                      case 'G': if(seq_pos < length){
+                                  seq[seq_pos++] = c;
+                                }
+                                break;
+                      default:  break;
+                    }
+                  }
+                  break;
+        default:  break;
       }
       ptr = strtok(NULL, " \t");
-      if (curr_int != -1)
+      if(curr_int != -1)
         last_int = curr_int;
-
       row++;
     }
 
     free(line);
-  } while (1);
 
-  if (seq) {
-    pairs                 = vrna_realloc(pairs, sizeof(plist) * (num_pairs + 1));
+  } while(1);
+
+  if(seq){
+    pairs = vrna_realloc(pairs, sizeof(plist) * (num_pairs+1));
     pairs[num_pairs].i    = 0;
     pairs[num_pairs].j    = 0;
     pairs[num_pairs].p    = 0.;
     pairs[num_pairs].type = 0;
 
 
-    /*
-     *  plist *p = pairs;
-     *  for(p = pairs; (*p).i; p++)
-     *    printf("pair (%d,%d)\n", (*p).i, (*p).j);
-     */
+/*
+    plist *p = pairs;
+    for(p = pairs; (*p).i; p++)
+      printf("pair (%d,%d)\n", (*p).i, (*p).j);
+*/
 
-    if (pkfree) {
-      float             mea, MEAgamma;
+    if(pkfree){
+      float mea, MEAgamma;
       MEAgamma = 2.0;
-      vrna_exp_param_t  *params = vrna_exp_params(NULL);
+      vrna_exp_param_t *params = vrna_exp_params(NULL);
 
       structure = (char *)vrna_alloc(sizeof(char) * (length + 1));
       strcpy(structure, seq);
 
       mea = MEA_seq(pairs, seq, structure, MEAgamma, params);
 
-      char  *structure_tmp  = vrna_db_from_plist(pairs, length);
-      int   d               =
-        vrna_bp_distance((const char *)structure, (const char *)structure_tmp);
-      if (verbose && (d > 0))
+      char *structure_tmp = vrna_db_from_plist(pairs, length);
+      int d =  vrna_bp_distance((const char *)structure, (const char *)structure_tmp);
+      if(verbose && (d > 0))
         fprintf(stderr, "removed %d pairs from pseudoknotted structure\n", d);
 
       free(structure_tmp);
@@ -190,10 +166,9 @@ main(int  argc,
     }
 
     printf("%s\n%s\n", seq, structure);
-    free(structure);
     free(pairs);
     free(seq);
   }
 
-  return EXIT_SUCCESS;
+  return(EXIT_SUCCESS);
 }
