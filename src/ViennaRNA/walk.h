27661bfc3154a008a089df938216b639458b02a1
Ronny Lorenz <ronny@tbi.univie.ac.at>
2019-04-12 13:56:50 +0200

    Restore backward compatibility for including old header walk.h

--- src/ViennaRNA/walk.h
+++ src/ViennaRNA/walk.h
@@ -12,6 +12,7 @@
 #warning "Including deprecated header file <ViennaRNA/walk.h>! Use <ViennaRNA/landscape/walk.h> instead!"
 # endif
 #include <ViennaRNA/landscape/walk.h>
+#include <ViennaRNA/landscape/neighbor.h>
 #endif
 
 #endif
