0b4604fc8b4e45b85a1898c1b456e02e40b13a87
Ronny Lorenz <ronny@tbi.univie.ac.at>
2020-06-05 00:01:56 +0200

    more dlib integration

---
+++ src/ViennaRNA/wrap_dlib.h
@@ -0,0 +1,17 @@
+#ifndef VIENNARNA_DLIB_WRAPPER_H
+#define VIENNARNA_DLIB_WRAPPER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "utils/basic.h"
+
+PUBLIC double  *
+vrna_equilibrium_conc(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
