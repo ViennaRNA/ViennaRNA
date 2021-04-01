03f4d020b657d6f5a15ba82b162d935082ee750d
fncnt <github@vncnt.eu>
2021-04-01 17:00:00 +0200

    Add a few simple tests to ensure that the changes did not break the API

---
+++ tests/python/test-RNA-ensemble_defect.py
@@ -0,0 +1,26 @@
+import RNApath
+
+RNApath.addSwigInterfacePath()
+
+
+import RNA
+import unittest
+
+
+seq          = "AGGAAACCUUAAUUGGUUA"
+structpk     = ".((...))(([[..))]]."
+
+class ensemble_defectTest(unittest.TestCase):
+    def test_ensemble_defect(self):
+        print "test_ensemble_defect"
+        fc = RNA.fold_compound(seq)
+        fc.pf()
+        ed = fc.ensemble_defect(structpk)
+        self.assertEqual(ed, 0.6140797258673892)
+        ed = fc.ensemble_defect(structpk, RNA.BRACKETS_ANY)
+        self.assertEqual(ed, 0.7279171755397522)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
