bbb24a6d029e7733c9ec45f67a60867c483fba0f
Ronny Lorenz <ronny@tbi.univie.ac.at>
2021-11-10 07:48:36 +0100

    CLEANUP: Uncrustify src/ViennaRNA/dist_vars.c

--- src/ViennaRNA/dist_vars.c
+++ src/ViennaRNA/dist_vars.c
@@ -1,8 +1,7 @@
 /*             Global variables for Distance-Package */
 
-int  edit_backtrack = 0;  /* calculate aligned representation */
+int   edit_backtrack = 0; /* calculate aligned representation */
 
-char *aligned_line[4];    /* containes the aligned string representations */
+char  *aligned_line[4];   /* containes the aligned string representations */
 
-int  cost_matrix = 0;     /* 0 for usual costs, 1 for Shapiro's costs */
-
+int   cost_matrix = 0;    /* 0 for usual costs, 1 for Shapiro's costs */
