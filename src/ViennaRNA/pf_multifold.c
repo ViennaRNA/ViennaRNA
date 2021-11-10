76bf3ac79c95ad62bd0c47b268f3ee0be672acd1
Ronny Lorenz <ronny@tbi.univie.ac.at>
2021-11-10 07:48:40 +0100

    CLEANUP: Uncrustify src/ViennaRNA/pf_multifold.c

--- src/ViennaRNA/pf_multifold.c
+++ src/ViennaRNA/pf_multifold.c
@@ -54,7 +54,7 @@ mf_rule_pair(vrna_fold_compound_t *fc,
   vrna_exp_param_t          *pf_params;
   vrna_md_t                 *md;
   vrna_callback_hc_evaluate *evaluate;
-  struct hc_ext_def_dat       hc_dat_local;
+  struct hc_ext_def_dat     hc_dat_local;
 
   contribution  = 0;
   S1            = fc->sequence_encoding;
@@ -76,43 +76,43 @@ mf_rule_pair(vrna_fold_compound_t *fc,
     s3    = (sn[i] == sn[i + 1]) ? S1[i + 1] : -1;
     qbase = vrna_exp_E_ext_stem(type, s5, s3, pf_params) *
             scale[2];
-    tmp   = 0.;
+    tmp = 0.;
 
-/*
-    if (evaluate(i + 1,
-                 j - 1,
-                 ends[sn[i]],
-                 ends[sn[i]] + 1,
-                 VRNA_DECOMP_EXT_EXT_EXT,
-                 &hc_dat_local))
-*/
+    /*
+     *  if (evaluate(i + 1,
+     *               j - 1,
+     *               ends[sn[i]],
+     *               ends[sn[i]] + 1,
+     *               VRNA_DECOMP_EXT_EXT_EXT,
+     *               &hc_dat_local))
+     */
     if (sn[i] != sn[i + 1]) {
       if ((sn[j - 1] != sn[j]) &&
-          (i + 1 == j)) {
+          (i + 1 == j))
         tmp = 1.;
-      } else if (sn[j - 1] == sn[j]) {
+      else if (sn[j - 1] == sn[j])
         tmp = q[my_iindx[i + 1] - j + 1];
-      }
     } else if (sn[j - 1] != sn[j]) {
       tmp = q[my_iindx[i + 1] - j + 1];
-    } else{
+    } else {
       tmp = q[my_iindx[i + 1] - ends[sn[i]]] *
             q[my_iindx[ends[sn[i]] + 1] - j + 1];
 
       /* check whether we find more strand nicks between i and j */
       nick = ends[sn[i]] + 1;
       while (sn[nick] != sn[j]) {
-/*
-        if (evaluate(i + 1,
-                     j - 1,
-                     ends[sn[nick]],
-                     ends[sn[nick]] + 1,
-                     VRNA_DECOMP_EXT_EXT_EXT,
-                     &hc_dat_local))
-*/
+        /*
+         *      if (evaluate(i + 1,
+         *                   j - 1,
+         *                   ends[sn[nick]],
+         *                   ends[sn[nick]] + 1,
+         *                   VRNA_DECOMP_EXT_EXT_EXT,
+         *                   &hc_dat_local))
+         */
         tmp2 = 1.;
         if (i + 1 <= ends[sn[nick]])
           tmp2 *= q[my_iindx[i + 1] - ends[sn[nick]]];
+
         if (ends[sn[nick]] + 1 <= j - 1)
           tmp2 *= q[my_iindx[ends[sn[nick]] + 1] - j + 1];
 
