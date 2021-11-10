9edb227a8e451b9079e52f6200139abad436ed1b
Ronny Lorenz <ronny@tbi.univie.ac.at>
2021-11-10 07:50:58 +0100

    CLEANUP: Uncrustify src/ViennaRNA/constraints/constraints.c

--- src/ViennaRNA/constraints/constraints.c
+++ src/ViennaRNA/constraints/constraints.c
@@ -20,41 +20,40 @@
 
 
 /*
-#################################
-# GLOBAL VARIABLES              #
-#################################
-*/
+ #################################
+ # GLOBAL VARIABLES              #
+ #################################
+ */
 
 /*
-#################################
-# PRIVATE VARIABLES             #
-#################################
-*/
+ #################################
+ # PRIVATE VARIABLES             #
+ #################################
+ */
 
 /*
-#################################
-# PRIVATE FUNCTION DECLARATIONS #
-#################################
-*/
+ #################################
+ # PRIVATE FUNCTION DECLARATIONS #
+ #################################
+ */
 
 /*
-#################################
-# BEGIN OF FUNCTION DEFINITIONS #
-#################################
-*/
-PUBLIC  void
-vrna_constraints_add( vrna_fold_compound_t *vc,
-                      const char *constraint,
-                      unsigned int options){
-
-  if(vc){
-    if(!vc->hc)
+ #################################
+ # BEGIN OF FUNCTION DEFINITIONS #
+ #################################
+ */
+PUBLIC void
+vrna_constraints_add(vrna_fold_compound_t *vc,
+                     const char           *constraint,
+                     unsigned int         options)
+{
+  if (vc) {
+    if (!vc->hc)
       vrna_hc_init(vc);
 
-    if(options & VRNA_CONSTRAINT_DB){ /* apply hard constraints from dot-bracket notation */
+    if (options & VRNA_CONSTRAINT_DB) /* apply hard constraints from dot-bracket notation */
       vrna_hc_add_from_db(vc, constraint, options);
-    } else { /* constraints from file is the default */
+    else                              /* constraints from file is the default */
       vrna_file_commands_apply(vc, constraint, VRNA_CMD_PARSE_HC | VRNA_CMD_PARSE_SC);
-    }
   }
 }
