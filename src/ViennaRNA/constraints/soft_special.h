d5c052f0c86782bff1822154a2482b236aa2a48b
Ronny Lorenz <ronny@tbi.univie.ac.at>
2022-06-27 17:12:24 +0200

    Add m6A parameter adjustments via soft constraints callbacks

---
+++ src/ViennaRNA/constraints/soft_special.h
@@ -0,0 +1,8 @@
+#ifndef VIENNA_RNA_PACKAGE_CONSTRAINTS_SOFT_SPECIAL_H
+#define VIENNA_RNA_PACKAGE_CONSTRAINTS_SOFT_SPECIAL_H
+
+void
+vrna_sc_m6A(vrna_fold_compound_t  *fc,
+            unsigned int          *modification_sites);
+
+#endif
