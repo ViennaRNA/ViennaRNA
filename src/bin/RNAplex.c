6b80ab1e9eabec07442e4ead564d579f28583c4c
Hua-Ting Yao <htyao@trill.tbi.univie.ac.at>
2023-03-02 23:41:18 +0100

    Complete cml tool, set saltInit as hidden argument

--- src/bin/RNAplex.c
+++ src/bin/RNAplex.c
@@ -280,6 +280,13 @@ main(int  argc,
   if (args_info.paramFile_given)
     ParamFile = strdup(args_info.paramFile_arg);
 
+  /* Salt concentration */
+  if (args_info.salt_given)
+    salt = args_info.salt_arg;
+
+  if (args_info.saltInit_given)
+    saltDPXInit = (int)(args_info.saltInit_arg * 100);
+
   /*binary*/
   if (args_info.binary_given)
     binaries = 1;
