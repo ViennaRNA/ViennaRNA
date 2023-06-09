f4581e2314852b412a3dad54a8166c8c52c91157
Ronny Lorenz <ronny@tbi.univie.ac.at>
2023-06-09 16:55:49 +0200

    SWIG: Add all RNAxplorer swig module dependencies to automake

--- src/RNAxplorer/interfaces/generic.mk
+++ src/RNAxplorer/interfaces/generic.mk
@@ -1,8 +1,20 @@
 
-SWIG_main_src = $(srcdir)/../RNAxplorer.i
+SWIG_main_src = \
+    $(srcdir)/../RNAxplorer.i
+
+SWIG_misc_src = \
+    $(srcdir)/../barrier_lower_bound.i \
+    $(srcdir)/../distorted_sampling.i \
+    $(srcdir)/../distorted_samplingMD.i \
+    $(srcdir)/../meshpoint.i \
+    $(srcdir)/../RNAwalk.i \
+    $(srcdir)/../paths.i
+
 
 SWIG_module_name = RNAxplorer
 
 SWIG_wrapper = RNAxplorer_wrap.cpp
 
-SWIG_src = $(SWIG_main_src) #$(SWIG_misc_src) $(SWIG_tmaps)
+SWIG_src = \
+    $(SWIG_main_src) \
+    $(SWIG_misc_src)
