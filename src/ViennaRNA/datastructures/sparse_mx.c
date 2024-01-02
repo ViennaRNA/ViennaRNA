eace79b69fb756f6c57e90fd95df2e66eabc8243
Ronny Lorenz <ronny@tbi.univie.ac.at>
2024-01-02 15:00:27 +0100

    Add (generic) compressed sparse row (CSR) matrix implementation

---
+++ src/ViennaRNA/datastructures/sparse_mx.c
@@ -0,0 +1,86 @@
+#include <stdlib.h>
+#include "ViennaRNA/utils/basic.h"
+#include "ViennaRNA/datastructures/sparse_mx.h"
+
+
+#define VRNA_SMX_CSR_DEFINE_INIT( TYPE )                                            \
+  vrna_smx_csr_##TYPE##_t *                                                         \
+  vrna_smx_csr_##TYPE##_init(unsigned int n)                                        \
+  {                                                                                 \
+    vrna_smx_csr_##TYPE##_t *mx = NULL;                                             \
+    if (n > 0) {                                                                    \
+      mx = (vrna_smx_csr_##TYPE##_t *)vrna_alloc(sizeof(vrna_smx_csr_##TYPE##_t));  \
+      mx->dirty = (unsigned char)1;                                                 \
+      vrna_array_init(mx->v);                                                       \
+      vrna_array_init(mx->col_idx);                                                 \
+      vrna_array_init(mx->row_idx);                                                 \
+      vrna_array_set_capacity(mx->row_idx, n + 1);                                  \
+    }                                                                               \
+    return mx;                                                                      \
+  }
+
+
+#define VRNA_SMX_CSR_DEFINE_FREE( TYPE )                  \
+  void                                                    \
+  vrna_smx_csr_##TYPE##_free(vrna_smx_csr_##TYPE##_t *mx) \
+  {                                                       \
+    if (mx) {                                             \
+      vrna_array_free(mx->v);                             \
+      vrna_array_free(mx->col_idx);                       \
+      vrna_array_free(mx->row_idx);                       \
+      free(mx);                                           \
+    }                                                     \
+  }
+
+
+/*  note, here we assume data to be stored arrives in an ordered way,
+ *  in particular row-wise (low to high) and columns low to high, in
+ *  that order!
+ */
+#define VRNA_SMX_CSR_DEFINE_INSERT( TYPE )      \
+  void  \
+  vrna_smx_csr_##TYPE##_insert(vrna_smx_csr_##TYPE##_t  *mx,  \
+                               unsigned int             i,    \
+                               unsigned int             j,    \
+                               TYPE                     e)    \
+  {                                                           \
+    vrna_array_append(mx->v, e);                              \
+    vrna_array_append(mx->col_idx, j);                        \
+    mx->row_idx[i + 1]++;                                     \
+  }
+
+
+#define VRNA_SMX_CSR_DEFINE_GET( TYPE )   \
+  TYPE  \
+  vrna_smx_csr_##TYPE##_get(vrna_smx_csr_##TYPE##_t *mx,        \
+                            unsigned int            i,          \
+                            unsigned int            j,          \
+                            TYPE                    default_v)  \
+  {                                                             \
+    unsigned int  p, d, s;                                      \
+    if (mx->dirty) {                                            \
+      for (s = 1; s < vrna_array_capacity(mx->row_idx); s++)    \
+        mx->row_idx[s] += mx->row_idx[s - 1];                   \
+      mx->dirty = (unsigned char)0;                             \
+    }                                                           \
+    s = mx->row_idx[i];                                         \
+    d = mx->row_idx[i + 1];                                     \
+    if (s < d) {                                                \
+      d -= s;                                                   \
+      for (p = 0; p < d; p++)                                   \
+        if (mx->col_idx[s + p] == j)                            \
+          return mx->v[s + p];                                  \
+    }                                                           \
+    return default_v;                                           \
+  }
+
+
+#define VRNA_SMX_CSR_DEFINE_ALL( TYPE )  \
+        VRNA_SMX_CSR_DEFINE_INIT(TYPE)   \
+        VRNA_SMX_CSR_DEFINE_FREE(TYPE)   \
+        VRNA_SMX_CSR_DEFINE_INSERT(TYPE) \
+        VRNA_SMX_CSR_DEFINE_GET(TYPE)
+
+
+VRNA_SMX_CSR_DEFINE_ALL(int)
+VRNA_SMX_CSR_DEFINE_ALL(float)
---
+++ src/ViennaRNA/datastructures/sparse_mx.h
@@ -0,0 +1,80 @@
+#ifndef   VRNA_SMX_H
+#define   VRNA_SMX_H
+
+#include <stdlib.h>
+#include <ViennaRNA/datastructures/array.h>
+
+
+#define VRNA_SMX_CSR( TYPE ) \
+  vrna_smx_csr_##TYPE##_t
+
+#define VRNA_SMX_CSR_DECLARE( TYPE )      \
+  typedef struct vrna_smx_csr_##TYPE##_s  \
+  {                                       \
+    unsigned char    dirty;               \
+    vrna_array(TYPE) v;                   \
+    vrna_array(unsigned int) col_idx;     \
+    vrna_array(unsigned int) row_idx;     \
+  } vrna_smx_csr_##TYPE##_t;
+
+
+#define VRNA_SMX_CSR_DECLARE_INIT( TYPE )     \
+  vrna_smx_csr_##TYPE##_t *                   \
+  vrna_smx_csr_##TYPE##_init(unsigned int n);
+
+
+#define VRNA_SMX_CSR_DECLARE_FREE( TYPE )                   \
+  void                                                      \
+  vrna_smx_csr_##TYPE##_free(vrna_smx_csr_##TYPE##_t *mx);
+
+
+/*  note, here we assume data to be stored arrives in an ordered way,
+ *  in particular row-wise (low to high) and columns low to high, in
+ *  that order!
+ */
+#define VRNA_SMX_CSR_DECLARE_INSERT( TYPE )      \
+  void  \
+  vrna_smx_csr_##TYPE##_insert(vrna_smx_csr_##TYPE##_t  *mx,  \
+                               unsigned int             i,    \
+                               unsigned int             j,    \
+                               TYPE                     e);
+
+
+#define VRNA_SMX_CSR_DECLARE_GET( TYPE )   \
+  TYPE  \
+  vrna_smx_csr_##TYPE##_get(vrna_smx_csr_##TYPE##_t *mx,        \
+                            unsigned int            i,          \
+                            unsigned int            j,          \
+                            TYPE                    default_v);
+
+
+#define VRNA_SMX_CSR_DECLARE_ALL( TYPE )  \
+        VRNA_SMX_CSR_DECLARE(TYPE)        \
+        VRNA_SMX_CSR_DECLARE_INIT(TYPE)   \
+        VRNA_SMX_CSR_DECLARE_FREE(TYPE)   \
+        VRNA_SMX_CSR_DECLARE_INSERT(TYPE) \
+        VRNA_SMX_CSR_DECLARE_GET(TYPE)
+
+
+#define GENERIC_SMX_CSR_PTR_FN_DEFINE(SMX_CSR_PTR, FN_NAME) \
+  _Generic((SMX_CSR_PTR), \
+      vrna_smx_csr_int_t *: (vrna_smx_csr_int ## _ ## FN_NAME), \
+      vrna_smx_csr_float_t *: (vrna_smx_csr_float ## _ ## FN_NAME))
+
+#define vrna_smx_csr_insert(X, i, j, v) \
+  GENERIC_SMX_CSR_PTR_FN_DEFINE((X), insert)((X), i, j, v)
+
+#define vrna_smx_csr_get(X, i, j, d) \
+  GENERIC_SMX_CSR_PTR_FN_DEFINE((X), get)((X), i, j, d)
+
+#define vrna_smx_csr_free(X) \
+  GENERIC_SMX_CSR_PTR_FN_DEFINE((X), free)((X))
+
+
+/* Below follows a list of sparse matrix declarations for different data types */
+VRNA_SMX_CSR_DECLARE_ALL(int)
+VRNA_SMX_CSR_DECLARE_ALL(float)
+
+
+
+#endif
