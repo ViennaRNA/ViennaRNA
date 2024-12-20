755e89d376fbfb86eebb67954b4091646a2f8d6c
Ronny Lorenz <ronny@tbi.univie.ac.at>
2024-12-20 14:26:39 +0100

    Allow for FASTA header changes in Utils/ct2db

--- src/Utils/ct2db.c
+++ src/Utils/ct2db.c
@@ -11,6 +11,7 @@
 #include "ViennaRNA/utils/basic.h"
 #include "ViennaRNA/structures/pairtable.h"
 #include "ViennaRNA/utils/log.h"
+#include "ViennaRNA/utils/strings.h"
 #include "ViennaRNA/params/basic.h"
 #include "ViennaRNA/io/utils.h"
 #include "ViennaRNA/io/file_formats.h"
@@ -20,9 +21,16 @@
 #include "ct2db_cmdl.h"
 
 typedef struct _parameters {
-  int no_pk;
-  int no_mod;
-  int verbose;
+  int           no_pk;
+  int           no_mod;
+  int           verbose;
+  unsigned int  id_from_filename;
+  char          *filename_suffix;
+  char          *id;
+  char          *id_prefix;
+  char          *id_suffix;
+  unsigned int  id_num;
+  unsigned int  id_num_start;
 } parameters;
 
 
@@ -58,6 +66,27 @@ remove_noncanonical_bases(char *sequence)
 }
 
 
+static const char *
+find_suffix(const char  *string,
+            const char  *suffix)
+{
+  size_t  l = 0;
+  size_t  n = 0;
+
+  l = strlen(suffix);
+  n = strlen(string);
+
+  if ((l) &&
+      (n) &&
+      (n >= l)) {
+    if (strcmp(string + (n - l), suffix) == 0)
+      return string + n - l;
+  }
+
+  return NULL;
+}
+
+
 int
 main(int  argc,
      char *argv[])
@@ -65,9 +94,16 @@ main(int  argc,
   struct ct2db_args_info  args_info;
   parameters              ct2db_params;
 
-  ct2db_params.no_pk    = 0;
-  ct2db_params.no_mod   = 0;
-  ct2db_params.verbose  = 0;
+  ct2db_params.no_pk            = 0;
+  ct2db_params.no_mod           = 0;
+  ct2db_params.verbose          = 0;
+  ct2db_params.id               = NULL;
+  ct2db_params.id_from_filename = 0;
+  ct2db_params.filename_suffix  = NULL;
+  ct2db_params.id_num           = 0;
+  ct2db_params.id_num_start     = 0;
+  ct2db_params.id_prefix        = NULL;
+  ct2db_params.id_suffix        = NULL;
 
   /*
    #############################################
@@ -87,6 +123,31 @@ main(int  argc,
   /* replace all characters by U or N */
   if (args_info.no_modified_given)
     ct2db_params.no_mod = 1;
+
+  if (args_info.fasta_header_given)
+    ct2db_params.id = strdup(args_info.fasta_header_arg);
+
+  if (args_info.fh_from_filename_given) {
+    switch (args_info.fh_from_filename_arg[0]) {
+      case 'a':
+        ct2db_params.id_from_filename = 2;
+        break;
+      case 'p':
+        ct2db_params.id_from_filename = 3;
+        break;
+      default:
+        ct2db_params.id_from_filename = 1;
+        break;
+    }
+  }
+
+  ct2db_params.filename_suffix = strdup(args_info.filename_suffix_arg);
+
+  if ((args_info.filename_suffix_given) &&
+      (strlen(args_info.filename_suffix_arg) == 0)) {
+    free(ct2db_params.filename_suffix);
+    ct2db_params.filename_suffix = NULL;
+  }
 
   char  **input_files = NULL;
   int   i, num_files = 0;
@@ -115,6 +176,40 @@ main(int  argc,
                       i + 1,
                       input_files[i]);
 
+        if (ct2db_params.id_from_filename) {
+          char *fn = vrna_basename(input_files[i]);
+
+          if (ct2db_params.filename_suffix) {
+            const char *p = find_suffix(fn, ct2db_params.filename_suffix);
+
+            if ((p) &&
+                (p > fn))
+              fn[p - fn] = '\0';
+          }
+
+          free(ct2db_params.id_prefix);
+          free(ct2db_params.id_suffix);
+          ct2db_params.id_prefix  = NULL;
+          ct2db_params.id_suffix  = NULL;
+
+          switch (ct2db_params.id_from_filename) {
+            case 1:
+              free(ct2db_params.id);
+              ct2db_params.id = fn;
+              break;
+            case 2:
+              ct2db_params.id_suffix = fn;
+              break;
+            case 3:
+              ct2db_params.id_prefix = fn;
+              break;
+            default:
+              break;
+          }
+
+          ct2db_params.id_num = ct2db_params.id_num_start;
+        }
+
         if (process_input(input_stream, (const char *)input_files[i], &ct2db_params) == 0)
           skip = 1;
 
@@ -126,6 +221,11 @@ main(int  argc,
   } else {
     (void)process_input(stdin, NULL, &ct2db_params);
   }
+
+  free(ct2db_params.filename_suffix);
+  free(ct2db_params.id);
+  free(ct2db_params.id_suffix);
+  free(ct2db_params.id_prefix);
 
   free(input_files);
 
@@ -187,10 +287,34 @@ process_input(FILE        *fp,
     if (opt->no_mod)
       remove_noncanonical_bases(sequence);
 
-    printf(">%s\n%s\n%s\n", (id) ? id : "seq", sequence, ss);
+    if (opt->id) {
+      free(id);
+      id = strdup(opt->id);
+    } else if (id == NULL) {
+      id = strdup("seq");
+    }
+
+    if (opt->id_num > opt->id_num_start) {
+      char *p = vrna_strdup_printf("%s_%u", id, opt->id_num);
+      free(id);
+      id = p;
+    }
+
+    if (opt->id_prefix) {
+      char *p = vrna_strdup_printf("%s %s", opt->id_prefix, id);
+      free(id);
+      id = p;
+    } else if (opt->id_suffix) {
+      char *p = vrna_strdup_printf("%s %s", id, opt->id_suffix);
+      free(id);
+      id = p;
+    }
+
+    printf(">%s\n%s\n%s\n", id, sequence, ss);
     free(id);
     free(sequence);
     free(ss);
+    opt->id_num++;
   }
 
   return 1;
